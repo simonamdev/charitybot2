f5aad45d43d7272c7158a5d9e9cba6db9d6faf2f
Simon AM <simon@agius-muscat.net>
2016-09-14 23:31:03 +0200

    Added several tests for the main scraper

--- services/sources/scraper/scraper.py
+++ services/sources/scraper/scraper.py
@@ -15,19 +15,20 @@ class Scraper:
         self.url = url
         self.parser = 'lxml'
         self.get = smokesignal.GetRequest(url=url, verbose=verbose)
+        self.url_is_valid = self.check_url_is_valid()
 
-    def is_url_valid(self):
-        self.get.make_request()
-        return self.get.get_response_code() == 200
-
-    def get_url_contents(self):
+    def check_url_is_valid(self):
         try:
             self.get.make_request()
         except smokesignal.exceptions.ConnectionFailedException:
             raise SourceUnavailableException('Scraper could not connect to url: {0}'.format(self.url))
+        return self.get.get_response_code() == 200
+
+    def get_url_contents(self):
+        self.get.make_request()
         return self.get.get_response_contents()
 
-    def soup_url_contents(self):
+    def get_soup_from_url(self):
         return BeautifulSoup(self.get_url_contents(), self.parser)
 
 
---
+++ services/sources/tests/test_scraper.py
@@ -0,0 +1,27 @@
+import pytest
+from bs4 import BeautifulSoup
+from scraper import scraper
+
+url = 'https://httpbin.org'
+
+
+def test_url_is_valid():
+    s = scraper.Scraper(url=url, verbose=True)
+    assert s.url_is_valid is True
+
+
+def test_non_empty_url_contents():
+    s = scraper.Scraper(url=url, verbose=True)
+    contents = s.get_url_contents()
+    assert '<!DOCTYPE html>' in contents
+
+
+def test_non_available_source_exception():
+    with pytest.raises(scraper.SourceUnavailableException):
+        s = scraper.Scraper(url='http://iojiowfjiojwiodjfowjdiojviunsduiv', verbose=True)
+
+
+def test_soup_valid_url_returns_bs4_object():
+    s = scraper.Scraper(url=url, verbose=True)
+    soup = s.get_soup_from_url()
+    assert isinstance(soup, type(BeautifulSoup('<h1>something</h1>', 'lxml')))
