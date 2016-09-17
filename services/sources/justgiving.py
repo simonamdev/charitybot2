3a6cfd2f5bbff101998174d5890841d89690a799
Simon AM <simon@agius-muscat.net>
2016-09-17 13:29:55 +0200

    Added first data source for justgiving scraper

--- services/sources/justgiving.py
+++ services/sources/justgiving.py
@@ -1,4 +1,4 @@
-from scraper.scraper import Scraper
+from scraper.scraper import Scraper, SoupDataSources
 
 
 class NoFundraiserNameGivenException(Exception):
@@ -12,11 +12,13 @@ class InvalidFundraiserUrlException(Exception):
 class JustGivingScraper(Scraper):
     def __init__(self, fundraiser_name='', verbose=False):
         self.fundraiser_name = fundraiser_name
+        self.soup_data_sources = None
         self.base_url = 'https://www.justgiving.com/'
         self.fundraiser_url = self.base_url + 'fundraising/' + fundraiser_name
         super().__init__(url=self.fundraiser_url, verbose=verbose)
         self.is_valid = False
         self.validate_scraper()
+        self.setup_soup_data_sources()
 
     def validate_scraper(self):
         if not self.fundraiser_name:
@@ -24,3 +26,12 @@ class JustGivingScraper(Scraper):
         if not self.is_url_valid():
             raise InvalidFundraiserUrlException('Fundraiser URL could not be validated. Make sure the name is correct?')
         self.is_valid = True
+
+    def setup_soup_data_sources(self):
+        sds = SoupDataSources()
+        sds.set_source(
+            source_name='amount_raised',
+            tag_type='span',
+            tag_class='statistics-amount-raised theme-highlight-text-font'
+        )
+        self.soup_data_sources = sds
