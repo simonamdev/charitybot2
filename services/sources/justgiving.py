f85802eaeac7724e5ed7b971970322338feb5c9d
Simon AM <simon@agius-muscat.net>
2016-09-18 10:20:10 +0200

    Added code and tests to get amount raised

--- services/sources/justgiving.py
+++ services/sources/justgiving.py
@@ -12,6 +12,8 @@ class InvalidFundraiserUrlException(Exception):
 class JustGivingScraper(Scraper):
     def __init__(self, fundraiser_name='', verbose=False):
         self.fundraiser_name = fundraiser_name
+        self.validate_fundraiser_name()
+        self.verbose = verbose
         self.soup_data_sources = None
         self.base_url = 'https://www.justgiving.com/'
         self.fundraiser_url = self.base_url + 'fundraising/' + fundraiser_name
@@ -20,11 +22,18 @@ class JustGivingScraper(Scraper):
         self.validate_scraper()
         self.setup_soup_data_sources()
 
-    def validate_scraper(self):
+    def print(self, print_string):
+        if self.verbose:
+            print('[JUSTGIVING] {0}'.format(print_string))
+
+    def validate_fundraiser_name(self):
         if not self.fundraiser_name:
             raise NoFundraiserNameGivenException('Cannot start a JustGiving object without a fundraiser name')
-        if not self.url_is_valid():
+
+    def validate_scraper(self):
+        if not self.url_is_valid:
             raise InvalidFundraiserUrlException('Fundraiser URL could not be validated. Make sure the name is correct?')
+        self.print('Just Giving Scraper successfully validated')
         self.is_valid = True
 
     def setup_soup_data_sources(self):
@@ -35,3 +44,17 @@ class JustGivingScraper(Scraper):
             tag_class='statistics-amount-raised theme-highlight-text-font'
         )
         self.soup_data_sources = sds
+        self.print('Just Giving Scraper sources initialised with {0} sources'.format(len(sds.get_available_source_names())))
+
+    def get_all_source_values(self):
+        for source_name in self.soup_data_sources.get_available_source_names():
+            self.print('Retrieiving the value of: {0}'.format(source_name))
+            value = self.get_source_value(source_name=source_name)
+            print(value)
+
+    def get_source_value(self, source_name):
+        self.get_soup_from_url()
+        return self.url_soup.find(
+            self.soup_data_sources.get_source_tag(source_name=source_name),
+            self.soup_data_sources.get_bs4_find_parameters_dict(source_name=source_name)
+        ).text
--- services/sources/tests/test_justgiving.py
+++ services/sources/tests/test_justgiving.py
@@ -27,4 +27,9 @@ def test_invalid_fundraiser_name_given_raises_exception():
 
 def test_get_amount_raised():
     jg = justgiving.JustGivingScraper(fundraiser_name=fundraiser_name, verbose=True)
+    amount_raised = jg.get_source_value(source_name='amount_raised')
+    # since the amount raised is not static, at least we can check for the £ and decimal point
+    assert amount_raised is not None
+    assert '£' in amount_raised
+    assert '.' in amount_raised
 
