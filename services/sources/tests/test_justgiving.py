1a8e3bfad7cb695ab3b4b483ed0f6c30c0b73111
Simon AM <simon@agius-muscat.net>
2016-09-18 09:40:35 +0200

    Fixed previous broken tests

--- services/sources/tests/test_justgiving.py
+++ services/sources/tests/test_justgiving.py
@@ -5,10 +5,13 @@ import justgiving
 def test_always_passes():
     assert True is True
 
+fundraiser_name = 'Disability-North'
 
-# This test might end up being invalid if the fundraiser expires and beomces a 404
+
+# This test might end up being invalid if the fundraiser expires and becomes a 404
 def test_new_valid_fundraiser():
-    jg = justgiving.JustGivingScraper(fundraiser_name='Disability-North', verbose=True)
+    print(fundraiser_name)
+    jg = justgiving.JustGivingScraper(fundraiser_name=fundraiser_name, verbose=True)
     assert jg.is_valid is True
 
 
@@ -19,4 +22,9 @@ def test_no_fundraiser_name_raises_exception():
 
 def test_invalid_fundraiser_name_given_raises_exception():
     with pytest.raises(justgiving.InvalidFundraiserUrlException):
-        justgiving.JustGivingScraper(fundraiser_name='isdofjisdjfiojsdfoijo')
+        justgiving.JustGivingScraper(fundraiser_name='isdofjisdjfiojsdfoijo', verbose=True)
+
+
+def test_get_amount_raised():
+    jg = justgiving.JustGivingScraper(fundraiser_name=fundraiser_name, verbose=True)
+
