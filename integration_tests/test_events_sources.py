903e60502384413d5cfbc314a4243de0a636234f
Simon AM <simon@agius-muscat.net>
2016-09-24 16:21:00 +0200

    Added integration test to confirm valid sources available

--- integration_tests/test_events_sources.py
+++ integration_tests/test_events_sources.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 import events.event_config as event_config
 import sources.sources
 
@@ -7,7 +6,6 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 config_directory = os.path.join(current_directory, 'configs')
 good_source_path = os.path.join(config_directory, 'good_source_event_config.json')
 bad_source_path = os.path.join(config_directory, 'bad_source_event_config.json')
-empty_source_path = os.path.join(config_directory, 'empty_source_event_config.json')
 
 sources_available = sources.sources.sources_available
 
@@ -19,5 +17,8 @@ def test_bad_sources_in_event_config_throws_exception():
         assert source not in bad_sources
 
 
-# def test_valid_sources_in_event_config_file():
-#     pass
+def test_valid_sources_in_event_config_file():
+    ec = event_config.EventConfiguration(file_path=good_source_path)
+    config_sources = ec.get_config_value(value_name='sources_required')
+    for source in config_sources:
+        assert source in sources_available
