84a49516f00823a110f9750839589d2d9b9af3c0
Simon AM <simon@agius-muscat.net>
2016-09-24 18:34:11 +0200

    Adjusted event class to use event config class

--- events/events.py
+++ events/events.py
@@ -1,3 +1,11 @@
-class Event:
-    def __init__(self, name, start_time, end_time):
+import events.event_config as event_config
 
+
+class Event:
+    def __init__(self, config_file_path):
+        self.config_file_path = config_file_path
+        self.config = None
+        self.read_config()
+
+    def read_config(self):
+        self.config = event_config.EventConfiguration(file_path=self.config_file_path)
