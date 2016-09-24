1c852f95dfa19d1eb6fdf45b092d48910e111ba3
Simon AM <simon@agius-muscat.net>
2016-09-24 16:26:18 +0200

    Cleaned up test config directory and tests

--- events/tests/test_event_config.py
+++ events/tests/test_event_config.py
@@ -4,11 +4,15 @@ from events import event_config
 from time import sleep
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-valid_config_path = os.path.join(current_directory, 'valid_test_config.json')
-invalid_json_config_path = os.path.join(current_directory, 'invalid_json_test_config.json')
-invalid_config_path = os.path.join(current_directory, 'invalid_test_config.json')
-empty_config_path = os.path.join(current_directory, 'empty_test_config.json')
-no_source_config_path = os.path.join(current_directory, 'empty_source_event_config.json')
+
+
+def get_config_file_path(config_name):
+    return os.path.join(current_directory, 'configs', config_name + '.' + event_config.EventConfiguration.config_format)
+# valid_config_path = os.path.join(current_directory, 'valid_test_config.json')
+# invalid_json_config_path = os.path.join(current_directory, 'invalid_json_test_config.json')
+# invalid_config_path = os.path.join(current_directory, 'invalid_test_config.json')
+# empty_config_path = os.path.join(current_directory, 'empty_test_config.json')
+# no_source_config_path = os.path.join(current_directory, 'empty_source_event_config.json')
 
 
 def test_event_config_does_not_exist():
@@ -17,24 +21,24 @@ def test_event_config_does_not_exist():
 
 
 def test_event_config_does_exist():
-    ec = event_config.EventConfiguration(file_path=valid_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('valid_config'))
     assert ec.config_exists() is True
 
 
 def test_empty_event_config():
-    ec = event_config.EventConfiguration(file_path=empty_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('empty_config'))
     with pytest.raises(event_config.InvalidEventConfigException):
         ec.read_config()
 
 
-def test_invalid_format_event_config():
-    ec = event_config.EventConfiguration(file_path=invalid_json_config_path)
+def test_invalid_formatted_event_config():
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('invalid_json_config'))
     with pytest.raises(event_config.InvalidEventConfigException):
         ec.read_config()
 
 
 def test_event_config_read_time_changing():
-    ec = event_config.EventConfiguration(file_path=valid_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('valid_config'))
     ec.read_config()
     first_read_time = ec.get_config_last_read_time()
     assert not first_read_time == 0
@@ -45,19 +49,19 @@ def test_event_config_read_time_changing():
 
 
 def test_valid_formatted_but_invalid_content_event_config():
-    ec = event_config.EventConfiguration(file_path=invalid_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('invalid_config'))
     with pytest.raises(event_config.InvalidEventConfigException):
         ec.read_config()
 
 
 def test_retrieving_non_existent_config_field_throws_exception():
-    ec = event_config.EventConfiguration(file_path=valid_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('valid_config'))
     with pytest.raises(event_config.EventConfigFieldDoesNotExistException):
         ec.get_config_value(value_name='test')
 
 
 def test_retrieving_integer_when_requesting_number_key_values():
-    ec = event_config.EventConfiguration(file_path=valid_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('valid_config'))
     values = []
     for key in ec.number_keys:
         values.append(ec.get_config_value(value_name=key))
@@ -66,7 +70,7 @@ def test_retrieving_integer_when_requesting_number_key_values():
 
 
 def test_retrieving_non_integer_when_requesting_non_number_key_values():
-    ec = event_config.EventConfiguration(file_path=valid_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('valid_config'))
     values = []
     all_keys = ec.keys_required
     non_number_keys = [key for key in all_keys if key not in ec.number_keys]
@@ -77,13 +81,13 @@ def test_retrieving_non_integer_when_requesting_non_number_key_values():
 
 
 def test_no_defined_sources_throwing_invalid_config_exception():
-    ec = event_config.EventConfiguration(file_path=no_source_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('empty_source_event_config'))
     with pytest.raises(event_config.InvalidEventConfigException):
         ec.read_config()
 
 
 def test_retrieving_list_when_requesting_lift_key_values():
-    ec = event_config.EventConfiguration(file_path=valid_config_path)
+    ec = event_config.EventConfiguration(file_path=get_config_file_path('valid_config'))
     values = []
     for key in ec.list_keys:
         values.append(ec.get_config_value(value_name=key))
