3af9d06cd466fdfbb63b431f0877bfea8df54161
Simon AM <simon@agius-muscat.net>
2016-10-06 13:41:34 +0200

    Added failing test for invalid event configuration loading in an event

--- tests/unit/test_events.py
+++ tests/unit/test_events.py
@@ -1,7 +1,15 @@
 import os
-import charitybot2.events.events as events
+import pytest
+from charitybot2.events.events import Event
+import charitybot2.storage.events_db as events_db
 import charitybot2.events.event_config as event_config
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-config_path = os.path.join(current_directory, 'configs', 'valid_config' + '.' + event_config.EventConfiguration.config_format)
+valid_config_path = os.path.join(current_directory, 'configs', 'valid_config' + '.' + event_config.EventConfiguration.config_format)
+invalid_config_path = os.path.join(current_directory, 'configs', 'invalid_config' + '.' + event_config.EventConfiguration.config_format)
 
+
+class TestEventConfigurationValidity:
+    def test_invalid_config_throws_exception(self):
+        with pytest.raises(event_config.InvalidEventConfigException):
+            e = Event(config_file_path=invalid_config_path)
