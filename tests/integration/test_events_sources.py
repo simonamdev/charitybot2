a42695833e9752aaccb7421bd60d31a43d5a5018
Simon AM <simon@agius-muscat.net>
2016-10-10 16:54:24 +0200

    Fixed broken tests

--- tests/integration/test_events_sources.py
+++ tests/integration/test_events_sources.py
@@ -8,18 +8,18 @@ config_directory = os.path.join(current_directory, 'configs')
 good_source_path = os.path.join(config_directory, 'good_source_event_config.json')
 bad_source_path = os.path.join(config_directory, 'bad_source_event_config.json')
 
-sources_available = sources.sources_available
+sources_available = sources.source_names_supported
 
 
 class TestEventSourcesValidity:
-    def test_bad_sources_in_event_config_throws_exception(self):
+    def test_bad_source_names_in_event_config_throws_exception(self):
         ec = event_config.EventConfiguration(file_path=bad_source_path)
         bad_sources = ec.get_config_value(value_name='sources_required')
-        for source in sources_available:
-            assert source not in bad_sources
+        for source in bad_sources:
+            assert source['name'] not in sources_available
 
-    def test_valid_sources_in_event_config_file(self):
+    def test_valid_source_names_in_event_config_file(self):
         ec = event_config.EventConfiguration(file_path=good_source_path)
         config_sources = ec.get_config_value(value_name='sources_required')
         for source in config_sources:
-            assert source in sources_available
+            assert source['name'] in sources_available
