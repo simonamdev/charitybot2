c48c37fa8d70f317001005a3834f69af1956f8cb
Simon AM <simon@agius-muscat.net>
2016-10-10 10:25:10 +0200

    Made test fail by default

--- tests/integration/test_events_storage.py
+++ tests/integration/test_events_storage.py
@@ -4,10 +4,12 @@ from charitybot2.events.events import Event
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 valid_config_path = os.path.join(current_directory, 'configs', 'good_source_event_config.json')
+test_db_path = os.path.join(current_directory, 'db', 'events.db')
+
 
 class TestEventRegistration:
-    # Move to integration test
     def test_register_new_event_successfully(self):
-        e = Event(config_file_path=valid_config_path, )
+        e = Event(config_file_path=valid_config_path, db_path=test_db_path)
         e.initialise_db_interface()
         e.register_event()
+        assert True is False
