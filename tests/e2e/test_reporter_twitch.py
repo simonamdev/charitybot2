abb5e428adb74e1ccd02f2b6a8627d2bf7291329
Simon AM <simon@agius-muscat.net>
2016-10-17 00:30:42 +0200

    Added basic selenium test structure

--- tests/e2e/test_reporter_twitch.py
+++ tests/e2e/test_reporter_twitch.py
@@ -1,4 +1,7 @@
 import pytest
+
+from selenium import webdriver
+
 from charitybot2.reporter.twitch import TwitchAccount, InvalidTwitchAccountException
 from charitybot2.reporter.twitch_config import client_id
 
@@ -10,3 +13,12 @@ class TestTwitchAccount:
 
     def test_twitch_account_exists(self):
         purrcat = TwitchAccount(name='purrcat259', token=client_id)
+
+
+class TestTwitchChat:
+    driver = webdriver.Chrome()
+
+    def test_twitch_chat_string_sent_appears(self):
+        self.driver.get('https://www.twitch.tv/purrcat259')
+        assert 'Purrcat259 - Twitch' == self.driver.title
+        self.driver.close()
