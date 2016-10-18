6963d3daf4dc15672dd3b2f02b9133d2ccb6ecb3
Simon AM <simon@agius-muscat.net>
2016-10-18 16:56:13 +0200

    Added sending log to DB option

--- charitybot2/charitybot2.py
+++ charitybot2/charitybot2.py
@@ -3,6 +3,11 @@ class BaseComponent:
         self.tag = tag
         self.verbose = verbose
 
-    def log(self, log_string):
+    def log(self, log_string, send_to_db=True):
         if self.verbose:
             print('[{0}] {1}'.format(self.tag, log_string))
+        if send_to_db:
+            self.send_to_db()
+
+    def send_to_db(self):
+        pass
