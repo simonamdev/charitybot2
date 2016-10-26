efb8560d335297b07227ab7ca97eb2a6bf95fc0a
Simon AM <simon@agius-muscat.net>
2016-10-26 09:04:46 +0200

    Switched to new twitch config class

--- charitybot2/reporter/twitch_config.py
+++ charitybot2/reporter/twitch_config.py
@@ -3,14 +3,18 @@ class IllegalConfigValueException(Exception):
 
 
 class TwitchConfig:
-    def __init__(self, client_id, client_secret):
+    def __init__(self, account_name, client_id, client_secret):
+        self.account_name = account_name
         self.client_id = client_id
         self.client_secret = client_secret
         self.validate_parameters()
 
     def validate_parameters(self):
-        if self.client_id == '' or self.client_secret == '':
+        if self.client_id == '' or self.client_secret == '' or self.account_name == '':
             raise IllegalConfigValueException
+
+    def get_account_name(self):
+        return self.account_name
 
     def get_client_id(self):
         return self.client_id
