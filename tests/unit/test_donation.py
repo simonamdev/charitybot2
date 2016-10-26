540220e155650698cd2ddd73337cd4c0b4cdaa05
Simon AM <simon@agius-muscat.net>
2016-10-26 13:43:52 +0200

    Lowercased method name

--- tests/unit/test_donation.py
+++ tests/unit/test_donation.py
@@ -53,6 +53,6 @@ class TestDonationProcessing:
         donation = Donation(old_amount=0, new_amount=1, timestamp=current_time_float)
         assert int(current_time_float) == donation.get_timestamp()
 
-    def test_default_currency_is_GBP(self):
+    def test_default_currency_is_gbp(self):
         donation = Donation(old_amount=0, new_amount=1)
         assert Currency.GBP is donation.get_donation_currency()
