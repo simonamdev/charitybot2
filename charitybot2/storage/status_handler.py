6cac117166c9681378d4c9ce099ad8c3c52fc11f
Simon AM <simon@agius-muscat.net>
2016-10-27 13:55:13 +0200

    Added retrieval of running events

--- charitybot2/storage/status_handler.py
+++ charitybot2/storage/status_handler.py
@@ -1,3 +1,9 @@
+from charitybot2.storage.events_db import EventMetadata
+
+
 class StatusHandler:
     def __init__(self, db_handler):
         self.db_handler = db_handler
+
+    def get_running_events(self):
+        return [row for row in self.db_handler.get_events_db().get_data_for_all_events() if row.get_state() == EventMetadata.ongoing_state]
--- tests/unit/test_status_handler.py
+++ tests/unit/test_status_handler.py
@@ -1,4 +1,5 @@
 from charitybot2.storage.db_handler import DBHandler
+from charitybot2.storage.events_db import EventMetadata
 from charitybot2.storage.status_handler import StatusHandler
 from tests.tests import TestFilePath, ResetDB
 
@@ -9,9 +10,15 @@ donations_db_init_script_path = TestFilePath().get_db_path('donations.sql')
 
 ResetDB(db_path=events_db_path, sql_path=events_db_init_script_path)
 ResetDB(db_path=donations_db_path, sql_path=donations_db_init_script_path)
-db_handler = DBHandler(events_db_path=events_db_path, donations_db_path=donations_db_path)
+db_handler = DBHandler(events_db_path=events_db_path, donations_db_path=donations_db_path, debug=True)
 
 
 class TestStatusHandlerValidity:
     def test_initialise_with_valid_handler_starts_ok(self):
         status_handler = StatusHandler(db_handler=db_handler)
+
+    def test_retrieving_registered_event_metadata(self):
+        status_handler = StatusHandler(db_handler=db_handler)
+        running_events = status_handler.get_running_events()
+        for metadata in running_events:
+            assert EventMetadata.ongoing_state == metadata.get_state()
