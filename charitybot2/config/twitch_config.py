d323699d1c8d110b8b402c00b290f25cf11d9f43
Simon AM <simon@agius-muscat.net>
2016-10-29 19:10:30 +0200

    Converted twitch config class to use JSON config super class

--- charitybot2/config/twitch_config.py
+++ charitybot2/config/twitch_config.py
@@ -1,17 +1,21 @@
-import os
+from charitybot2.config.json_config import JSONConfigurationFile
 
 
-class TwitchAccountConfigurationFileDoesNotExistException(Exception):
-    pass
-
-
-class TwitchAccountConfiguration:
-    config_format = 'JSON'
+class TwitchAccountConfiguration(JSONConfigurationFile):
+    keys_required = (
+        'name',
+        'id',
+        'secret'
+    )
 
     def __init__(self, file_path):
-        self.file_path = file_path
-        if not self.config_exists():
-            raise TwitchAccountConfigurationFileDoesNotExistException
+        super().__init__(file_path=file_path, keys_required=self.keys_required)
 
-    def config_exists(self):
-        return os.path.isfile(self.file_path)
+    def get_account_name(self):
+        return self.config_data['name']
+
+    def get_client_id(self):
+        return self.config_data['id']
+
+    def get_client_secret(self):
+        return self.config_data['secret']
--- tests/unit/test_twitch_config.py
+++ tests/unit/test_twitch_config.py
@@ -1,40 +1,25 @@
-import pytest
-from charitybot2.config.twitch_config import TwitchAccountConfiguration, TwitchAccountConfigurationFileDoesNotExistException
+from charitybot2.config.twitch_config import TwitchAccountConfiguration
 from tests.tests import TestFilePath
 
 
 def get_config_file_path(config_name):
-    return TestFilePath().get_config_path('twitch', config_name + '.' + TwitchAccountConfiguration.config_format)
+    return TestFilePath().get_config_path('twitch', config_name + '.json')
 
 
 class TestTwitchAccountConfigExistence:
-    def test_twitch_account_config_does_not_exist_throws_exception(self):
-        with pytest.raises(TwitchAccountConfigurationFileDoesNotExistException):
-            tac = TwitchAccountConfiguration(file_path='dsoijoisjfd')
-
     def test_twitch_account_config_does_exist(self):
-        tac = TwitchAccountConfiguration(file_path=get_config_file_path('valid_twitch_config'))
+        tac = TwitchAccountConfiguration(file_path=get_config_file_path('test_twitch_config'))
         assert True is tac.config_exists()
 
-#
-#
-# class TestTwitchConfigValidity:
-#     def test_passing_empty_client_id_throws_exception(self):
-#         with pytest.raises(IllegalConfigValueException):
-#             TwitchAccountConfiguration('name', client_id='', client_secret='secret')
-#
-#     def test_passing_empty_client_secret_throws_exception(self):
-#         with pytest.raises(IllegalConfigValueException):
-#             TwitchAccountConfiguration('name', client_id='id', client_secret='')
-#
-#     def test_passing_empty_account_name_throws_exception(self):
-#         with pytest.raises(IllegalConfigValueException):
-#             TwitchAccountConfiguration('', 'id', 'secret')
-#
-#
-# class TestTwitchReturningValues:
-#     def test_getting_values(self):
-#         tc = TwitchAccountConfiguration('name', client_id='id', client_secret='secret')
-#         assert 'name' == tc.get_account_name()
-#         assert 'id' == tc.get_client_id()
-#         assert 'secret' == tc.get_client_secret()
+
+class TestTwitchAccountConfigRetrieve:
+    tac = TwitchAccountConfiguration(file_path=get_config_file_path('test_twitch_config'))
+
+    def test_get_name(self):
+        assert 'name' == self.tac.get_account_name()
+
+    def test_get_id(self):
+        assert 'id' == self.tac.get_client_id()
+
+    def test_get_secret(self):
+        assert 'secret' == self.tac.get_client_secret()
