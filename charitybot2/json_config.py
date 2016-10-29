61976eb118e4c8ce34c1d5367f9a55f99815a77b
Simon AM <simon@agius-muscat.net>
2016-10-29 11:39:57 +0200

    Added config file reading and basic validation

--- charitybot2/json_config.py
+++ charitybot2/json_config.py
@@ -5,10 +5,17 @@ class ConfigurationFileDoesNotExistException(Exception):
     pass
 
 
+class InvalidConfigurationException(Exception):
+    pass
+
+
 class JSONConfigurationFile:
     def __init__(self, file_path):
         self.file_path = file_path
         self.confirm_config_exists()
+        self.config_data = None
+        self.read_config()
+        self.validate_config_data()
 
     def confirm_config_exists(self):
         if not self.config_exists():
@@ -16,3 +23,11 @@ class JSONConfigurationFile:
 
     def config_exists(self):
         return os.path.isfile(self.file_path)
+
+    def validate_config_data(self):
+        if self.config_data == '':
+            raise InvalidConfigurationException('Configuration File is empty')
+
+    def read_config(self):
+        with open(self.file_path, 'r') as config_file:
+            self.config_data = config_file.read()
--- tests/unit/test_json_config.py
+++ tests/unit/test_json_config.py
@@ -1,13 +1,24 @@
 import pytest
-from charitybot2.json_config import ConfigurationFileDoesNotExistException, JSONConfigurationFile
+from charitybot2.json_config import ConfigurationFileDoesNotExistException, JSONConfigurationFile, InvalidConfigurationException
 from tests.tests import TestFilePath
 
 
 def get_config_file_path(config_name):
-    return TestFilePath().get_config_path('', config_name + '.json')
+    return TestFilePath().get_config_path('base', config_name + '.json')
 
 
 class TestConfigFileExistence:
-    def test_config_files_does_not_exist_throws_exception(self):
+    def test_config_file_does_not_exist_throws_exception(self):
         with pytest.raises(ConfigurationFileDoesNotExistException):
             conf = JSONConfigurationFile(file_path='fgdfgeg')
+
+    def test_config_file_exists(self):
+        conf = JSONConfigurationFile(file_path=get_config_file_path('valid_config'))
+        assert True is conf.config_exists()
+
+
+class TestConfigValidity:
+    def test_empty_config_file_throws_exception(self):
+        with pytest.raises(InvalidConfigurationException):
+            conf = JSONConfigurationFile(file_path=get_config_file_path('empty_config'))
+
