19602371852de4b06fc5c6ee554e06d625031a17
Simon AM <simon@agius-muscat.net>
2016-10-29 19:04:35 +0200

    Moved production files to data folder

--- charitybot2/paths.py
+++ charitybot2/paths.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 # Databases
-production_logs_db_path = os.path.join(os.path.dirname(__file__), 'db', 'logs.db')
-production_donations_db_path = os.path.join(os.path.dirname(__file__), 'db', 'donations.db')
+production_logs_db_path = os.path.join(os.path.dirname(__file__), 'data', 'logs.db')
+production_donations_db_path = os.path.join(os.path.dirname(__file__), 'data', 'donations.db')
 
 # Services
 mocksite_path = os.path.join(os.path.dirname(__file__), 'sources', 'mocks', 'mocksite.py')
