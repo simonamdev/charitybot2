a60aaf120b06b04b74d0bd4e431986d5a22a30e1
Simon AM <simon@agius-muscat.net>
2016-11-01 18:26:58 +0100

    Added UUIDs to test strings to avoid testing for old strings

--- tests/e2e/test_reporter_twitch.py
+++ tests/e2e/test_reporter_twitch.py
@@ -1,3 +1,4 @@
+import uuid
 from time import sleep
 
 from charitybot2.reporter.purrbot_config import purrbot_config
@@ -5,10 +6,14 @@ from charitybot2.reporter.twitch import TwitchAccount, TwitchChatBot
 from selenium import webdriver
 
 
-class TestTwitchChat:
-    test_string = 'Charitybot2 automated E2E test string'
+def return_unique_test_string():
+    base_string = 'Charitybot2 automated E2E test string [{}]'
+    return base_string.format(str(uuid.uuid4()))
 
+
+class TestTwitchChat:
     def test_twitch_chat_string_sent_appears(self):
+        test_string = return_unique_test_string()
         driver = webdriver.Chrome()
         driver.get('https://www.twitch.tv/purrcat259')
         assert 'Purrcat259 - Twitch' == driver.title
@@ -18,10 +23,10 @@ class TestTwitchChat:
             twitch_account=bot_account,
             verbose=True
         )
-        bot.quick_post_in_channel(self.test_string)
+        bot.quick_post_in_channel(test_string)
         sleep(4)
         chat_window = driver.find_element_by_class_name('chat-display')
         print(chat_window.text)
-        assert self.test_string in chat_window.text
+        assert test_string in chat_window.text
         driver.close()
 
