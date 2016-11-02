0988254bacd0e8ae6b10995753e9d55418e0bc34
Simon AM <simon@agius-muscat.net>
2016-11-02 13:57:14 +0100

    Added basic api call

--- charitybot2/reporter/status_console/js/api.js
+++ charitybot2/reporter/status_console/js/api.js
@@ -2,4 +2,11 @@ class API {
     constructor(url) {
         this._url = url;
     }
+
+    setupEventInformation(eventName) {
+        var api_url = this._url + 'events/' + eventName;
+        $.getJSON(api_url, (data)=> {
+            console.log(data);
+        });
+    }
 };
