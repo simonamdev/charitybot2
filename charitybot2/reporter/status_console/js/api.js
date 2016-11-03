4f9f4aa92cff965eabe5a1b894fa1edeaeb559e1
Simon AM <simon@agius-muscat.net>
2016-11-03 09:37:58 +0100

    Added loading spinner

--- charitybot2/reporter/status_console/js/api.js
+++ charitybot2/reporter/status_console/js/api.js
@@ -4,6 +4,7 @@ class API {
     }
 
     setupEventInformation(eventName) {
+        showLoader();
         var api_url = this._url + 'event/' + eventName;
         $.getJSON(api_url, (data)=> {
             console.log(data);
@@ -11,6 +12,7 @@ class API {
             $('#donation_count').text(data['donation_count']);
             $('#donation_average').text(data['donation_average']);
             $('#largest_donation').text(data['largest_donation']);
+            hideLoader();
         });
     }
 };
---
+++ charitybot2/reporter/status_console/js/ui.js
@@ -0,0 +1,9 @@
+function showLoader() {
+    $('#loader').show();
+    $('#content').hide();
+}
+
+function hideLoader() {
+    $('#loader').hide();
+    $('#content').show();
+}
