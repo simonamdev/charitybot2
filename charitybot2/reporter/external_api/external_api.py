a0c043928e3366fc5a138899a4b918ea1b2ee06b
Simon AM <simon@agius-muscat.net>
2016-11-05 01:32:17 +0100

    Removed limit check print

--- charitybot2/reporter/external_api/external_api.py
+++ charitybot2/reporter/external_api/external_api.py
@@ -57,7 +57,6 @@ def event_donations(event_name):
     all_donations = donations_db.get_all_donations(event_name=event_name)
     # TODO: Add option to limit via Neopysqlite and not slicing after the fact
     limit = request.args.get('limit')
-    print(limit)
     if limit is not None:
         all_donations = all_donations[-int(limit):]
     donation_objects = [
