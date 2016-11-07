96604866d601eb3cef4ef924602bcb89b0794328
Simon AM <simon@agius-muscat.net>
2016-11-07 12:53:07 +0100

    Adjusted paths to new directory structure

--- charitybot2/paths.py
+++ charitybot2/paths.py
@@ -2,13 +2,17 @@ import os
 
 current_directory = os.path.dirname(__file__)
 data_folder = os.path.join(current_directory, 'data')
+config_folder = os.path.join(data_folder, 'config')
+twitch_config_folder = os.path.join(config_folder, 'twitch')
+event_config_folder = os.path.join(config_folder, 'event')
+db_folder = os.path.join(data_folder, 'db')
 
 # Configs
-purrcat259_config_path = os.path.join(data_folder, 'purrcat259.json')
+purrcat259_config_path = os.path.join(twitch_config_folder, 'purrcat259.json')
 
 # Databases
-production_logs_db_path = os.path.join(data_folder, 'logs.db')
-production_donations_db_path = os.path.join(data_folder, 'donations.db')
+production_logs_db_path = os.path.join(db_folder, 'logs.db')
+production_donations_db_path = os.path.join(db_folder, 'donations.db')
 
 # Services
 mocksite_path = os.path.join(current_directory, 'sources', 'mocks', 'mocksite.py')
