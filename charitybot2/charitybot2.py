5aa4e3b65763b97b6b336e4c82bced0983d05e5c
Simon AM <simon@agius-muscat.net>
2016-11-08 12:54:31 +0100

    Added basic startup validation

--- charitybot2/charitybot2.py
+++ charitybot2/charitybot2.py
@@ -1,2 +1,16 @@
-class BotStartupValidation:
+import os
+
+
+class MissingRequiredFolderException(Exception):
     pass
+
+
+class BotStartupValidator:
+    def __init__(self, db_directory, config_directory):
+        self.db_directory = db_directory
+        self.config_directory = config_directory
+        self.confirm_directories_exist()
+
+    def confirm_directories_exist(self):
+        if not os.path.isdir(self.config_directory) or not os.path.isdir(self.db_directory):
+            raise MissingRequiredFolderException('Either config or DB directories are missing')
---
+++ tests/unit/test_startup.py
@@ -0,0 +1,13 @@
+import pytest
+from charitybot2.charitybot2 import BotStartupValidator, MissingRequiredFolderException
+from tests.tests import TestFilePath
+
+
+class TestStartupValidation:
+    def test_checking_folder_that_does_not_exist_throws_exception(self):
+        with pytest.raises(MissingRequiredFolderException):
+            validator = BotStartupValidator(db_directory='bla', config_directory='foo')
+        with pytest.raises(MissingRequiredFolderException):
+            validator = BotStartupValidator(db_directory=TestFilePath().db_dir, config_directory='car')
+        with pytest.raises(MissingRequiredFolderException):
+            validator = BotStartupValidator(db_directory='coo', config_directory=TestFilePath().config_dir)
