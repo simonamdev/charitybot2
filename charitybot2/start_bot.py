b9b6d71301419fa4d9b51090d60e68682e67f711
Simon AM <simon@agius-muscat.net>
2016-11-08 14:02:10 +0100

    Added basic argument parsing

--- charitybot2/start_bot.py
+++ charitybot2/start_bot.py
@@ -1,11 +1,17 @@
-import click
+import argparse
 
 
-@click.command()
-@click.argument('')
+def setup_argument_parser():
+    parser = argparse.ArgumentParser(description='Charity stream bot')
+    parser.add_argument('--event-config', type=str, help='Event Config file name')
+    parser.add_argument('--twitch-config', type=str, help='Twitch Bot Config file name')
+    return parser
+
+
 def start_bot():
-    pass
-
+    args = setup_argument_parser().parse_args()
+    print(args)
+    print(args.event_config)
 
 if __name__ == '__main__':
     start_bot()
