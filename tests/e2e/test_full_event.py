7f8276c3901f1a3618d8ae2fdbf60d27fc0c95a3
Simon AM <simon@agius-muscat.net>
2016-11-13 16:43:18 +0100

    Added failing E2E test

--- tests/e2e/test_full_event.py
+++ tests/e2e/test_full_event.py
@@ -1,10 +1,14 @@
+import time
+from charitybot2.charitybot2 import CharityBot, create_parser
+from charitybot2.events.currency import Currency
 from charitybot2.paths import mocksite_path
+from charitybot2.reporter.twitch import ChatBot
 from selenium import webdriver
-from tests.e2e.test_reporter_twitch import navigate_to_twitch_channel
 from tests.integration.test_event_loop_with_mocksite import MockEvent
 from tests.tests import ServiceTest
 
 driver = None
+parser = create_parser()
 service_test = ServiceTest(
     service_name='Donations Mocksite',
     service_url=MockEvent.mocksite_base_url,
@@ -12,11 +16,23 @@ service_test = ServiceTest(
     enter_debug=False)
 
 
+# Duplicated from other E2E test, could do with a common refactor
+def navigate_to_twitch_channel():
+    global driver
+    driver.get('https://www.twitch.tv/purrcat259')
+    assert 'Purrcat259 - Twitch' == driver.title
+    time.sleep(2)
+
+
+def get_twitch_chat_box_contents():
+    return driver.find_element_by_class_name('chat-display').text
+
+
 def setup_module():
     service_test.start_service()
     global driver
     driver = webdriver.Chrome()
-    driver.implicity_wait(5)
+    driver.implicitly_wait(5)
 
 
 def teardown_module():
@@ -26,8 +42,15 @@ def teardown_module():
 
 
 class TestFullTwitchEvent:
-    navigate_to_twitch_channel()
-
+    def test_full_twitch_event(self):
+        navigate_to_twitch_channel()
+        args = parser.parse_args(['config', '--debug', '--twitch-config', 'purrcat259'])
+        bot = CharityBot(args=args)
+        bot.initialise_bot()
+        bot.start_bot()
+        time.sleep(4)
+        expected_string = ChatBot.donation_string.format(Currency.GBP, 50.0, Currency.GBP, 250.52)
+        assert expected_string in get_twitch_chat_box_contents()
 
 
 class TestFullAPIEvent:
