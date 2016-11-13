3792a680b0c42f0133c8855f749a439f6952fd08
Simon AM <simon@agius-muscat.net>
2016-11-13 12:04:11 +0100

    Added tests for existing configurations

--- tests/integration/test_startup_validation.py
+++ tests/integration/test_startup_validation.py
@@ -11,12 +11,24 @@ class TestParserRequiredArguments:
         with pytest.raises(IllegalArgumentException):
             Startup(args=args)
 
-    def test_passing_non_existent_event_configs_throws_exception(self):
+    def test_passing_non_existent_event_config_throws_exception(self):
         args = self.parser.parse_args(['test', '--debug'])
         with pytest.raises(MissingRequiredFileException):
             Startup(args=args)
+
+    def test_passing_existent_event_config(self):
+        args = self.parser.parse_args(['config', '--debug'])
+        Startup(args=args)
+
+
+class TestParserOptionalArguments:
+    parser = create_parser()
 
     def test_passing_non_existent_twitch_config_in_twitch_mode_throws_exception(self):
         args = self.parser.parse_args(['config', '--debug', '--twitch-config', 'bla'])
         with pytest.raises(MissingRequiredFileException):
             Startup(args=args)
+
+    def test_passing_existent_twitch_config_in_twitch_mode(self):
+        args = self.parser.parse_args(['config', '--debug', '--twitch-config', 'test_twitch_config'])
+        Startup(args=args)
