cbbf20bcfa7b37fcc2fef6fb277e7e3ee882fd95
Simon AM <simon@agius-muscat.net>
2016-11-17 13:44:10 +0100

    Change CSS according to API call status

--- charitybot2/reporter/external_api/static/js/update.js
+++ charitybot2/reporter/external_api/static/js/update.js
@@ -3,18 +3,47 @@ class OverlayUpdate {
         this._eventName = eventName;
     }
 
+    setCurrency() {
+        var eventDetailsUrl = 'http://127.0.0.1:9000/event/' + this._eventName;
+        $.getJSON(eventDetailsUrl, (data) => {
+            $('#currency_symbol').text(data['currency_symbol']);
+        }).fail((data) => {
+            console.error(data['error']);
+        })
+    }
+
     runUpdateCycle() {
         var lastDonationUrl = 'http://127.0.0.1:9000/event/' + this._eventName + '/donations/last';
+        this.setPendingCSS();
         $.getJSON(lastDonationUrl, (data) => {
+            this.setAvailableCSS();
             $('#amount_raised').text(data['total_raised']);
         }).fail((data) => {
-            $('#amount_raised').text(data['error']);
+            this.setUnavailableCSS();
         });
     }
 
+    setCurrencyAmountCSS(cssClass) {
+        $('#currency_symbol').attr('class', cssClass);
+        $('#amount_raised').attr('class', cssClass);
+    }
+
+    setAvailableCSS() {
+        this.setCurrencyAmountCSS('available');
+    }
+
+    setPendingCSS() {
+        this.setCurrencyAmountCSS('pending');
+    }
+
+    setUnavailableCSS() {
+        this.setCurrencyAmountCSS('unavailable');
+    }
+
     setupLoop() {
+        this.setCurrency();
         setInterval(() => {
             this.runUpdateCycle();
-        }, 2000);
+        }, 4000);
     }
 }
