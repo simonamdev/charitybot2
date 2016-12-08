e78bf0b06d4bc9ff0e7a13336bc391d77338d8b2
Simon AM <simon@agius-muscat.net>
2016-12-08 21:02:50 +0100

    Moved overlay update commands to HTML file

--- charitybot2/reporter/external_api/static/js/update.js
+++ charitybot2/reporter/external_api/static/js/update.js
@@ -1,49 +1,19 @@
-class OverlayUpdate {
-    constructor(eventName) {
-        this._eventName = eventName;
-    }
+function setCurrency(eventName) {
+    var eventDetailsUrl = 'http://127.0.0.1:9000/event/' + eventName;
+    $.getJSON(eventDetailsUrl, (data) => {
+        console.log(data);
+        $('#currency_symbol').text(data['currency_symbol']);
+    }).fail((data) => {
+        console.error('Unable to set currency');
+    })
+}
 
-    setCurrency() {
-        var eventDetailsUrl = 'http://127.0.0.1:9000/event/' + this._eventName;
-        $.getJSON(eventDetailsUrl, (data) => {
-            $('#currency_symbol').text(data['currency_symbol']);
-        }).fail((data) => {
-            console.error('Unable to set currency');
-        })
-    }
-
-    runUpdateCycle() {
-        this.setCurrency();
-        var lastDonationUrl = 'http://127.0.0.1:9000/event/' + this._eventName + '/donations/last';
-        this.setPendingCSS();
-        $.getJSON(lastDonationUrl, (data) => {
-            this.setAvailableCSS();
-            $('#amount_raised').text(data['total_raised']);
-        }).fail((data) => {
-            this.setUnavailableCSS();
-        });
-    }
-
-    setCurrencyAmountCSS(cssClass) {
-        $('#currency_symbol').attr('class', cssClass);
-        $('#amount_raised').attr('class', cssClass);
-    }
-
-    setAvailableCSS() {
-        this.setCurrencyAmountCSS('available');
-    }
-
-    setPendingCSS() {
-        this.setCurrencyAmountCSS('pending');
-    }
-
-    setUnavailableCSS() {
-        this.setCurrencyAmountCSS('unavailable');
-    }
-
-    setupLoop() {
-        setInterval(() => {
-            this.runUpdateCycle();
-        }, 4000);
-    }
+function setDonationAmount(eventName) {
+    var lastDonationUrl = 'http://127.0.0.1:9000/event/' + eventName + '/donations/last';
+    $.getJSON(lastDonationUrl, (data) => {
+        console.log(data);
+        $('#amount_raised').text(data['total_raised']);
+    }).fail((data) => {
+        console.error('Unable to set amount raised');
+    })
 }
