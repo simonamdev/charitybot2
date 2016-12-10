db48a3eb23a8a18c365b5c2106513bff9c870680
Simon AM <simon@agius-muscat.net>
2016-12-10 20:27:38 +0100

    Actually start the bot when running from CLI

--- cb2.py
+++ cb2.py
@@ -1,4 +1,6 @@
 from charitybot2.charitybot2 import create_parser, CharityBot
 
 args = create_parser().parse_args()
-CharityBot(args=args)
+bot = CharityBot(args=args)
+bot.initialise_bot()
+bot.start_bot()
