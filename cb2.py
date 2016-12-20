7e0a1f49ba0815164b4525eb882c161358852e38
Simon AM <simon@agius-muscat.net>
2016-12-20 09:59:21 +0100

    Changed parser method name

--- cb2.py
+++ cb2.py
@@ -1,6 +1,6 @@
-from charitybot2.charitybot2 import create_parser, CharityBot
+from charitybot2.charitybot2 import create_cb_process_parser, CharityBot
 
-args = create_parser().parse_args()
+args = create_cb_process_parser().parse_args()
 bot = CharityBot(args=args)
 bot.initialise_bot()
 bot.start_bot()
--- tests/integration/test_startup_validation.py
+++ tests/integration/test_startup_validation.py
@@ -1,9 +1,9 @@
 import pytest
-from charitybot2.charitybot2 import MissingRequiredFileException, CharityBot, create_parser, IllegalArgumentException
+from charitybot2.charitybot2 import MissingRequiredFileException, CharityBot, create_cb_process_parser, IllegalArgumentException
 
 
 class TestParserRequiredArguments:
-    parser = create_parser()
+    parser = create_cb_process_parser()
 
     def test_passing_empty_event_name_throws_exception(self):
         args = self.parser.parse_args(['', '--debug'])
@@ -21,7 +21,7 @@ class TestParserRequiredArguments:
 
 
 class TestParserOptionalArguments:
-    parser = create_parser()
+    parser = create_cb_process_parser()
 
     def test_passing_non_existent_twitch_config_in_twitch_mode_throws_exception(self):
         args = self.parser.parse_args(['config', '--debug', '--twitch-config', 'bla'])
