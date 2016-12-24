3211b73de3ccf697bff487d091823bb462b79c39
Simon AM <simon@agius-muscat.net>
2016-12-24 18:23:17 +0100

    Added clearer failure state

--- cb2.py
+++ cb2.py
@@ -1,6 +1,11 @@
 from charitybot2.charitybot2 import create_cb_process_parser, CharityBot
+from charitybot2.events.event import EventAlreadyFinishedException
 
 args = create_cb_process_parser().parse_args()
 bot = CharityBot(args=args)
-bot.initialise_bot()
+try:
+    bot.initialise_bot()
+except EventAlreadyFinishedException:
+    print('Event end time is in the past - check your configuration file')
+    quit(1)
 bot.start_bot()
