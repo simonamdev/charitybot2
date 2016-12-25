4ccb0ee8e7668e4c3cff00b169e708ee2d0b7a69
Simon AM <simon@agius-muscat.net>
2016-12-26 00:18:13 +0100

    Added more realistic behaviour for load testing

--- tests/performance/locustfile.py
+++ tests/performance/locustfile.py
@@ -4,12 +4,23 @@ event_name = 'performance'
 api_path = '/api/v1/'
 overlay_path = '/overlay/' + event_name
 stats_path = '/stats/' + event_name
+static_path = '/static/'
+
+static_files = [
+    'js/ui.js',
+    'js/stats_console.js',
+    'css/spinner.css',
+    'css/main.css'
+]
+
+stats_api_calls = [
+    'event/' + event_name + '/donations/distribution',
+    'event/' + event_name + '/donations',
+    'event/' + event_name + '/donations/info'
+]
 
 
 class ApiBehaviour(TaskSet):
-    def on_start(self):
-        pass
-
     @task(1)
     def index(self):
         self.client.get(api_path)
@@ -18,12 +29,24 @@ class ApiBehaviour(TaskSet):
     def overlay(self):
         self.client.get(overlay_path)
 
-    @task(3)
+
+class UserBehaviour(TaskSet):
+    @task
     def stats(self):
         self.client.get(stats_path)
+        for file in static_files:
+            self.client.get(static_path + file)
+        for call in stats_api_calls:
+            self.client.get(api_path + call)
 
 
-class ApiUser(HttpLocust):
+class ApiCalls(HttpLocust):
     task_set = ApiBehaviour
     min_wait = 5000
     max_wait = 9000
+
+
+class OverlayCalls(HttpLocust):
+    task_set = UserBehaviour
+    min_wait = 5000
+    max_wait = 30000
