54a18a2c3cda9788192cb540870ed5b899c69312
Simon AM <simon@agius-muscat.net>
2016-12-26 19:43:11 +0100

    Fixed integration tests

--- tests/integration/test_event_currency.py
+++ tests/integration/test_event_currency.py
@@ -1,3 +1,4 @@
+from charitybot2.botconfig.event_config import EventConfigurationFromFile
 from charitybot2.events.currency import Currency
 from charitybot2.events.event import Event
 from charitybot2.storage.db_handler import DBHandler
@@ -9,7 +10,8 @@ donations_db_init_script_path = TestFilePath().get_db_path('donations.sql')
 
 ResetDB(db_path=donations_db_path, sql_path=donations_db_init_script_path)
 db_handler = DBHandler(donations_db_path=donations_db_path, debug=True)
-valid_event = Event(config_path=valid_config_path, db_handler=db_handler)
+valid_event_config = EventConfigurationFromFile(file_path=valid_config_path)
+valid_event = Event(event_configuration=valid_event_config, db_handler=db_handler)
 
 
 class TestEventCurrency:
--- tests/integration/test_event_loop_with_mocksite.py
+++ tests/integration/test_event_loop_with_mocksite.py
@@ -1,15 +1,16 @@
 import time
 
 import requests
+from charitybot2.botconfig.event_config import EventConfigurationFromFile, EventConfigurationCreator
 from charitybot2.events.event_loop import EventLoop
 from charitybot2.events.event import Event
 from charitybot2.paths import mocksite_path
 from charitybot2.sources.mocks.mocksite import mocksite_full_url
 from charitybot2.storage.db_handler import DBHandler
 from tests.tests import ResetDB, ServiceTest, TestFilePath
-from tests.unit.test_event_loop import ValidTestEvent
 
-config_path = TestFilePath().get_config_path('event', 'config.json')
+config_path = TestFilePath().get_config_path('event', 'valid_config.json')
+config_values = EventConfigurationFromFile(file_path=config_path).get_config_data()
 donations_db_path = TestFilePath().get_db_path('donations.db')
 donations_db_init_script_path = TestFilePath().get_db_path('donations.sql')
 
@@ -18,7 +19,12 @@ class MockEvent(Event):
     mocksite_base_url = mocksite_full_url
 
     def __init__(self, mock_name, mock_end_time):
-        super().__init__(config_path=config_path, db_handler=DBHandler(donations_db_path=donations_db_path, debug=True))
+        config_values['end_time'] = mock_end_time
+        mock_event_config = EventConfigurationCreator(config_values=config_values).get_event_configuration()
+        super().__init__(
+            event_configuration=mock_event_config,
+            db_handler=DBHandler(donations_db_path=donations_db_path,
+            debug=True))
         self.mock_name = mock_name
         self.mock_end_time = mock_end_time
 
@@ -54,18 +60,18 @@ def teardown_module():
 
 class TestEventRunThrough:
     def test_getting_new_amount_properly_formatted(self):
-        test_event = MockEvent('test_one', time.time() + 5)
+        test_event = MockEvent('test_one', int(time.time()) + 5)
         el = EventLoop(event=test_event, debug=True)
         assert 200.52 == el.get_new_amount()
 
     def test_event_cycles_increment_properly(self):
-        test_event = MockEvent('test_two', time.time() + 5)
+        test_event = MockEvent('test_two', int(time.time()) + 5)
         test_event_loop = EventLoop(event=test_event, debug=True)
         test_event_loop.start()
         assert 1 == test_event_loop.loop_count
 
     def test_event_amount_raised_changes_each_cycle(self):
-        test_event = MockEvent('test_three', time.time() + 10)
+        test_event = MockEvent('test_three', int(time.time()) + 10)
         # first reset the amount on the mocksite so that the amount raised is back to default
         test_event.reset_mocksite()
         test_event_loop = EventLoop(event=test_event, debug=True)
