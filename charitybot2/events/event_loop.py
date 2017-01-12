45e2c5b40aa3561d4c8ff09249308d9baa19e666
Simon AM <simon@agius-muscat.net>
2017-01-12 17:14:18 +0100

    Fixed broken E2E test

--- charitybot2/events/event_loop.py
+++ charitybot2/events/event_loop.py
@@ -37,7 +37,7 @@ class EventLoop:
             self.event.set_amount_raised(last_donation.get_total_raised())
             self.logger.log_info('Amount raised retrieved from database is: {}{}'.format(
                 self.event.get_currency().get_symbol(),
-                self.event.scrape_amount_raised()
+                self.event.get_amount_raised()
             ))
 
     def donations_already_present(self):
