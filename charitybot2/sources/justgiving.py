88e2b747ede752fb2b3577e9a65dfb77abd31edc
Simon AM <simon@agius-muscat.net>
2017-01-12 11:57:58 +0100

    Cleaned up Justgiving object

--- charitybot2/sources/justgiving.py
+++ charitybot2/sources/justgiving.py
@@ -1,4 +1,5 @@
 from charitybot2.sources.url_call import ConnectionFailedException
+from charitybot2.storage.logger import Logger
 
 from .scraper import Scraper, SoupDataSources, SourceUnavailableException
 
@@ -14,17 +15,14 @@ class InvalidFundraiserUrlException(Exception):
 class JustGivingScraper(Scraper):
     def __init__(self, url, debug=False):
         self.url = url
-        self.verbose = debug
+        self.debug = debug
+        self.logger = Logger(source='JustGivingScraper', event='', console_only=self.debug)
         self.soup_data_sources = None
         try:
             super().__init__(url=self.url, debug=debug)
         except SourceUnavailableException:
             raise InvalidFundraiserUrlException
         self.setup_soup_data_sources()
-
-    def print(self, print_string):
-        if self.verbose:
-            print('[JUSTGIVING] {0}'.format(print_string))
 
     def setup_soup_data_sources(self):
         sds = SoupDataSources()
@@ -34,11 +32,12 @@ class JustGivingScraper(Scraper):
             tag_class='statistics-amount-raised theme-highlight-text-font'
         )
         self.soup_data_sources = sds
-        self.print('Just Giving Scraper sources initialised with {0} sources'.format(len(sds.get_available_source_names())))
+        self.logger.log_info('Just Giving Scraper sources initialised with {0} sources'.format(
+            len(sds.get_available_source_names())))
 
     def get_all_source_values(self):
         for source_name in self.soup_data_sources.get_available_source_names():
-            self.print('Retrieiving the value of: {0}'.format(source_name))
+            self.logger.log_verbose('Retrieiving the value of: {0}'.format(source_name))
             value = self.get_source_value(source_name=source_name)
             print(value)
 
--- tests/api/test_justgiving.py
+++ tests/api/test_justgiving.py
@@ -1,6 +1,6 @@
 import pytest
-import charitybot2.sources.justgiving as justgiving
 from charitybot2.paths import mocksite_path
+from charitybot2.sources.justgiving import JustGivingScraper
 from charitybot2.sources.scraper import SourceUnavailableException
 from tests.integration.test_event_loop_with_mocksite import MockEvent
 from tests.tests import ServiceTest
@@ -15,13 +15,13 @@ mocksite = ServiceTest(
 class TestFundraiserRetrieve:
     def test_get_amount_raised_from_mocksite(self):
         mocksite.start_service()
-        jg = justgiving.JustGivingScraper(url=MockEvent.mocksite_base_url + 'justgiving/', debug=True)
+        jg = JustGivingScraper(url=MockEvent.mocksite_base_url + 'justgiving/', debug=True)
         amount_raised = jg.get_source_value(source_name='amount_raised')
         assert '£100.52' == amount_raised
         mocksite.stop_service()
 
     def test_get_amount_raised_from_actual_url(self):
-        jg = justgiving.JustGivingScraper(url='https://www.justgiving.com/fundraising/alasdair-clift', debug=True)
+        jg = JustGivingScraper(url='https://www.justgiving.com/fundraising/alasdair-clift', debug=True)
         amount_raised = jg.get_source_value(source_name='amount_raised')
         # since the amount raised is not static, at least we can check for the £ and decimal point
         assert amount_raised is not None
@@ -30,7 +30,7 @@ class TestFundraiserRetrieve:
 
     def test_get_amount_raised_fails_gracefully(self):
         mocksite.start_service()
-        jg = justgiving.JustGivingScraper(url=MockEvent.mocksite_base_url, debug=True)
+        jg = JustGivingScraper(url=MockEvent.mocksite_base_url, debug=True)
         # simulate as if the website went down
         mocksite.stop_service()
         with pytest.raises(SourceUnavailableException):
