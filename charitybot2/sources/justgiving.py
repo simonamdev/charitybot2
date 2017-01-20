350bb2868602af6a3ebe8c7d78e4a4fb05ef1dc3
Simon AM <simon@agius-muscat.net>
2017-01-20 15:56:06 +0100

    Separated fundraising and campaign into separate classes

--- charitybot2/sources/justgiving.py
+++ charitybot2/sources/justgiving.py
@@ -13,9 +13,9 @@ class InvalidFundraiserUrlException(Exception):
 
 
 class JustGivingScraper(Scraper):
-    def __init__(self, url, debug=False):
+    def __init__(self, url, scraper_type, debug=False):
         self.url = url
-        self.type = self.__determine_type()
+        self.scraper_type = scraper_type
         self.debug = debug
         self.logger = Logger(source='JustGivingScraper', event='', console_only=self.debug)
         self.soup_data_sources = None
@@ -23,39 +23,29 @@ class JustGivingScraper(Scraper):
             super().__init__(url=self.url, debug=debug)
         except SourceUnavailableException:
             raise InvalidFundraiserUrlException
-        self.__setup_soup_data_sources()
-
-    def __determine_type(self):
-        if 'fundraising' in self.url:
-            return 'fundraiser'
-        elif 'campaign' in self.url:
-            return 'campaign'
-        else:
-            raise InvalidFundraiserUrlException('Unable to discern fundraiser type from the Just Giving URL')
+        self.logger.log_info('JustGiving Scraper URL: {}'.format(self.url))
 
     def get_type(self):
-        return self.type
-
-    def __setup_soup_data_sources(self):
-        self.soup_data_sources = SoupDataSources()
-        if self.get_type() == 'fundraiser':
-            self.soup_data_sources.set_source(
-                source_name='amount_raised',
-                tag_type='span',
-                tag_class='statistics-amount-raised theme-highlight-text-font'
-            )
-        elif self.get_type() == 'campaign':
-            self.soup_data_sources.set_source(
-                source_name='amount_raised',
-                tag_type='p',
-                tag_class='dna-text-brand-l jg-theme-text TotalDonation__totalRaised___1sUPY'
-            )
-        self.logger.log_info('JustGiving Scraper sources initialised with {0} sources'.format(
-            len(self.soup_data_sources.get_available_source_names())))
-        self.logger.log_info('JustGiving URL: {}'.format(self.url))
+        return self.scraper_type
 
     def scrape_amount_raised(self):
-        return self.get_source_value(source_name='amount_raised')
+        pass
+
+
+class JustGivingFundraisingScraper(JustGivingScraper):
+    def __init__(self, url, debug):
+        super().__init__(url=url, scraper_type='fundraising', debug=debug)
+        self.soup_data_sources = self.__setup_soup_data_sources()
+
+    def __setup_soup_data_sources(self):
+        sds = SoupDataSources()
+        sds.set_source(
+            source_name='amount_raised',
+            tag_type='span',
+            tag_class='statistics-amount-raised theme-highlight-text-font')
+        self.logger.log_info('JustGiving Fundraising Scraper initialised with: {0} sources'.format(
+            len(sds.get_available_source_names())))
+        return sds
 
     def __get_source_value_from_url_contents(self, url_contents, source_name):
         try:
@@ -77,3 +67,33 @@ class JustGivingScraper(Scraper):
             raise SourceUnavailableException('Unable to connect to the source')
         return source_value
 
+    def scrape_amount_raised(self):
+        return self.get_source_value(source_name='amount_raised')
+
+
+class JustGivingCampaignScraper(JustGivingScraper):
+    pass
+
+# elif self.get_type() == 'campaign':
+# self.soup_data_sources.set_source(
+#     source_name='amount_raised',
+#     tag_type='p',
+#     tag_class='dna-text-brand-l jg-theme-text TotalDonation__totalRaised___1sUPY'
+# )
+
+
+class JustGivingScraperCreator:
+    def __init__(self, url, debug=False):
+        self.url = url
+        self.debug = debug
+
+    def get_scraper(self):
+        return self.__determine_scraper_type()
+
+    def __determine_scraper_type(self):
+        if 'fundraising' in self.url:
+            return JustGivingFundraisingScraper(url=self.url, debug=self.debug)
+        elif 'campaign' in self.url:
+            return JustGivingCampaignScraper(url=self.url, debug=self.debug)
+        else:
+            raise InvalidFundraiserUrlException('Unable to discern fundraiser type from the JustGiving URL')
--- tests/api/test_justgiving_against_mocksite.py
+++ tests/api/test_justgiving_against_mocksite.py
@@ -1,5 +1,5 @@
 import pytest
-from charitybot2.sources.justgiving import JustGivingScraper, InvalidFundraiserUrlException
+from charitybot2.sources.justgiving import JustGivingScraper, InvalidFundraiserUrlException, JustGivingScraperCreator
 from charitybot2.sources.mocks.mocksite import mock_justgiving_fundraising_url, mock_justgiving_campaign_url
 from charitybot2.sources.scraper import SourceUnavailableException
 from charitybot2.sources.url_call import UrlCall, ConnectionFailedException
@@ -22,23 +22,11 @@ def teardown_module():
         pass
 
 
-class TestJustGivingType:
-    def test_default_type_is_fundraiser(self):
-        jg = JustGivingScraper(url='https://www.justgiving.com/fundraising/FrontierDev', debug=True)
-        assert 'fundraiser' == jg.get_type()
-
-    def test_given_campaign_url_type_is_campaign(self):
-        jg = JustGivingScraper(url='https://www.justgiving.com/campaigns/charity/specialeffect/gameblast17', debug=True)
-        assert 'campaign' == jg.get_type()
-
-    def test_given_incorrect_url_throws_exception(self):
-        with pytest.raises(InvalidFundraiserUrlException):
-            jg = JustGivingScraper(url='https://www.justgiving.com/blabla/something', debug=True)
-
-
-class TestJustGivingScraping:
+class TestJustGivingFundraisingScraper:
     def test_get_amount_raised_from_actual_url(self):
-        jg = JustGivingScraper(url='https://www.justgiving.com/fundraising/FrontierDev', debug=True)
+        jg = JustGivingScraperCreator(
+            url='https://www.justgiving.com/fundraising/FrontierDev',
+            debug=True).get_scraper()
         amount_raised = jg.get_source_value(source_name='amount_raised')
         # since the amount raised is not static, at least we can check for the £ and decimal point
         assert amount_raised is not None
@@ -46,12 +34,9 @@ class TestJustGivingScraping:
         assert '.' in amount_raised or ',' in amount_raised
 
     def test_get_amount_raised_from_fundraiser_page(self):
-        jg = JustGivingScraper(url=mock_justgiving_fundraising_url, debug=True)
-        amount_raised = jg.get_source_value(source_name='amount_raised')
-        assert '£100.52' == amount_raised
-
-    def test_get_amount_raised_from_campaign_page(self):
-        jg = JustGivingScraper(url=mock_justgiving_campaign_url, debug=True)
+        jg = JustGivingScraperCreator(
+            url=mock_justgiving_fundraising_url,
+            debug=True).get_scraper()
         amount_raised = jg.get_source_value(source_name='amount_raised')
         assert '£100.52' == amount_raised
 
@@ -59,8 +44,28 @@ class TestJustGivingScraping:
         # only start  the mocksite if it is not running
         if not UrlCall(url=mock_justgiving_fundraising_url).get().status_code == 200:
             mock_fundraising_website.start()
-        jg = JustGivingScraper(url=mock_justgiving_fundraising_url, debug=True)
+        jg = JustGivingScraperCreator(
+            url=mock_justgiving_fundraising_url,
+            debug=True).get_scraper()
         # simulate as if the website went down
         mock_fundraising_website.stop()
         with pytest.raises(SourceUnavailableException):
             amount_raised = jg.scrape_amount_raised()
+
+
+class TestJustGivingCampaignScraper:
+    @pytest.mark.skip(reason='Need a valid old campaign URL to use')
+    def test_get_amount_raised_from_actual_url(self):
+        url = ''
+        jg = JustGivingScraperCreator(
+            url=mock_justgiving_campaign_url,
+            debug=True).get_scraper()
+        amount_raised = jg.get_source_value(source_name='amount_raised')
+        assert '£100.52' == amount_raised
+
+    def test_get_amount_raised_from_campaign_page(self):
+        jg = JustGivingScraperCreator(
+            url=mock_justgiving_campaign_url,
+            debug=True).get_scraper()
+        amount_raised = jg.get_source_value(source_name='amount_raised')
+        assert '£100.52' == amount_raised
