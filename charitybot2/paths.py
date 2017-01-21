372f74a78de7c39203c44c23046c11f23574c78d
Simon AM <simon@agius-muscat.net>
2017-01-21 10:32:26 +0100

    Changed empty api key file to fake api key file

--- charitybot2/paths.py
+++ charitybot2/paths.py
@@ -7,6 +7,9 @@ config_folder = os.path.join(data_folder, 'config')
 twitch_config_folder = os.path.join(config_folder, 'twitch')
 event_config_folder = os.path.join(config_folder, 'event')
 db_folder = os.path.join(data_folder, 'db')
+test_folder = os.path.join(root_directory, 'tests')
+test_data_folder = os.path.join(test_folder, 'data')
+test_config_folder = os.path.join(test_data_folder, 'config')
 
 # Configs
 purrcat259_config_path = os.path.join(twitch_config_folder, 'purrcat259.json')
@@ -23,5 +26,8 @@ external_api_script_path = os.path.join(current_directory, 'reporter', 'external
 # Other file paths
 status_console_file_path = os.path.join(current_directory, 'reporter', 'status_console', 'console.html')
 user_agents_file_path = os.path.join(current_directory, 'sources', 'user_agents.txt')
-empty_justgiving_api_key_path = os.path.join(current_directory, 'api', 'empty_justgiving_api_key.txt')
-cb2_justgiving_api_key_path = os.path.join(current_directory, 'api', 'cb2_justgiving_api_key.txt')
+empty_justgiving_api_key_path = os.path.join(config_folder, 'api', 'empty_justgiving_api_key.txt')
+cb2_justgiving_api_key_path = os.path.join(config_folder, 'api', 'cb2_justgiving_api_key.txt')
+
+# Test file paths
+debug_justgiving_api_key_path = os.path.join(test_config_folder, 'api', 'debug_justgiving_api_key.txt')
