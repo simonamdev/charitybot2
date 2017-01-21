4376e2a5e019785becf1a39c9b31f0950703db75
Simon AM <simon@agius-muscat.net>
2017-01-21 22:12:05 +0100

    Parametrised integration test

--- tests/integration/test_event_loop_with_mocksite.py
+++ tests/integration/test_event_loop_with_mocksite.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 from charitybot2.botconfig.event_config import EventConfigurationFromFile, EventConfigurationCreator
 from charitybot2.events.event_loop import EventLoop
 from charitybot2.events.event import Event
@@ -16,6 +17,7 @@ class MockEvent(Event):
         config_values['source_url'] = mock_justgiving_fundraising_url
         config_values['internal_name'] = mock_name
         config_values['end_time'] = mock_end_time
+        config_values['update_delay'] = 2
         mock_event_config = EventConfigurationCreator(config_values=config_values).get_event_configuration()
         super().__init__(event_configuration=mock_event_config, db_path=repository_db_path)
         self.mock_name = mock_name
@@ -50,23 +52,39 @@ class TestEventRunThrough:
         mock_fundraising_website.increase_amount()
         assert 200.52 == el.get_new_amount()
 
-    def test_event_cycles_increment_properly(self):
-        test_event = MockEvent(config_path=valid_config_path, mock_name='test_two', mock_end_time=int(time.time()) + 5)
+    @pytest.mark.parametrize('cycle_count', [
+        1,
+        2,
+        3
+    ])
+    def test_event_cycles_increment_properly(self, cycle_count):
+        test_event = MockEvent(
+            config_path=valid_config_path,
+            mock_name='test_two',
+            mock_end_time=int(time.time()) + (2 * cycle_count))
         test_event_loop = EventLoop(event=test_event, debug=True)
         test_event_loop.start()
-        assert 1 == test_event_loop.loop_count
+        assert cycle_count == test_event_loop.loop_count
 
-    def test_event_amount_raised_changes_each_cycle(self):
-        test_event = MockEvent(config_path=valid_config_path, mock_name='test_three', mock_end_time=int(time.time()) + 10)
+    @pytest.mark.parametrize('cycle_count', [
+        1,
+        2,
+        3
+    ])
+    def test_event_amount_raised_changes_each_cycle(self, cycle_count):
+        increment_amount = 50
+        test_event = MockEvent(
+            config_path=valid_config_path,
+            mock_name='test_three',
+            mock_end_time=int(time.time()) + (2 * cycle_count))
         # first reset the amount on the mocksite so that the amount raised is back to default
         mock_fundraising_website.reset_amount()
-        # 3 cycles
-        mock_fundraising_website.increase_amount()
-        mock_fundraising_website.increase_amount()
-        mock_fundraising_website.increase_amount()
+        for i in range(0, cycle_count):
+            mock_fundraising_website.increase_amount()
         test_event_loop = EventLoop(event=test_event, debug=True)
         test_event_loop.start()
-        assert 250.52 == test_event_loop.event.get_amount_raised()
+        expected_amount_raised = round(100.52 + (increment_amount * cycle_count), 2)
+        assert expected_amount_raised == test_event_loop.event.get_amount_raised()
 
     def test_event_amount_raising_only_when_amount_is_different(self):
         test_event = MockEvent(config_path=valid_config_path, mock_name='event_loop_test', mock_end_time=int(time.time()) + 10)
