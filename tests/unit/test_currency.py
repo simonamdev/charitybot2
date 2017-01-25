4ab1ebe5fe960f036ddae149f421ff9683cf0dd2
Simon AM <simon@agius-muscat.net>
2017-01-25 13:14:28 +0100

    Added tests for currency class

--- tests/unit/test_currency.py
+++ tests/unit/test_currency.py
@@ -1,6 +1,22 @@
-class TestCurrencyInstantiation:
-    def test_retrieve_key(self):
-        pass
+import pytest
+from charitybot2.models.currency import Currency
 
-    def test_retrieve_symbol(self):
-        pass
+
+class TestCurrencyInstantiation:
+    @pytest.mark.parametrize('key', [
+        'GBP',
+        'USD',
+        'EUR'
+    ])
+    def test_retrieve_key(self, key):
+        currency = Currency(key=key)
+        assert key == currency.get_key()
+
+    @pytest.mark.parametrize('key,symbol', [
+        ('GBP', '£'),
+        ('USD', '$'),
+        ('EUR', '€')
+    ])
+    def test_retrieve_symbol(self, key, symbol):
+        currency = Currency(key=key)
+        assert symbol == currency.get_symbol()
