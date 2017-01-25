1f3aeb8318ef5c1be634df1750e91bb18559acf8
Simon AM <simon@agius-muscat.net>
2017-01-25 23:14:39 +0100

    Added donation class

--- charitybot2/models/donation.py
+++ charitybot2/models/donation.py
@@ -1,2 +1,47 @@
-class Donation:
+import time
+
+
+class InvalidDonationException(Exception):
     pass
+
+
+class Donation:
+    def __init__(self, amount, timestamp=int(time.time()), identifier=None, event_identifier=None):
+        self._amount = amount
+        self._timestamp = timestamp
+        self._identifier = identifier
+        self._event_identifier = event_identifier
+        self.__validate_donation()
+        self.__parse_donation_amount()
+
+    def __validate_donation(self):
+        if self._amount == '':
+            raise InvalidDonationException('Donation amount cannot be an empty string')
+        if self._amount == 0:
+            raise InvalidDonationException('Donation cannot have a value of 0')
+        if self._timestamp < 0:
+            raise InvalidDonationException('Invalid timestamp passed')
+
+    def __parse_donation_amount(self):
+        if isinstance(self._amount, str):
+            self._amount = self._amount.replace(',', '')
+            try:
+                self._amount = float(self._amount)
+            except ValueError:
+                raise InvalidDonationException('Cannot parse input string properly')
+
+    @property
+    def amount(self):
+        return self._amount
+
+    @property
+    def timestamp(self):
+        return self._timestamp
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    @property
+    def event_identifier(self):
+        return self._event_identifier
--- tests/unit/test_donation.py
+++ tests/unit/test_donation.py
@@ -1,15 +1,36 @@
+import pytest
+from charitybot2.models.donation import Donation, InvalidDonationException
+
+test_donation = Donation(
+    amount=50,
+    timestamp=999999,
+    identifier='identifier',
+    event_identifier='event_identifier')
+
+
 class TestDonationInstantiation:
-    def test_retrieve_identifier(self):
-        pass
+    @pytest.mark.parametrize('expected,actual', [
+        (50, test_donation.amount),
+        (999999, test_donation.timestamp),
+        ('identifier', test_donation.identifier),
+        ('event_identifier', test_donation.event_identifier)
+    ])
+    def test_retrieval(self, expected, actual):
+        assert expected == actual
 
-    def test_retrieve_amount(self):
-        pass
+    def test_passing_amount_string_with_commas_parses_properly(self):
+        donation_amount = '12,345.52'
+        donation = Donation(amount=donation_amount, timestamp=0)
+        assert 12345.52 == donation.amount
 
-    def test_retrieve_timestamp(self):
-        pass
 
-    def test_retrieve_currency(self):
-        pass
-
-    def test_retrieve_event_identifier(self):
-        pass
+class TestDonationExceptions:
+    @pytest.mark.parametrize('amount,timestamp,identifier,event_identifier', [
+        (0, 0, None, None),
+        (50, -20, None, None),
+        ('', 0, None, None),
+        ('foobar', 0, None, None)
+    ])
+    def test_incorrect_values_throws_exception(self, amount, timestamp, identifier, event_identifier):
+        with pytest.raises(InvalidDonationException):
+            Donation(amount=amount, timestamp=timestamp, identifier=identifier, event_identifier=event_identifier)
