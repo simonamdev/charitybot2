10896e74e54bdd82995cd3735676cd706c1a5159
Simon AM <simon@agius-muscat.net>
2017-01-26 14:07:10 +0100

    Added retrieval tests for event class

--- tests/unit/test_event.py
+++ tests/unit/test_event.py
@@ -1,24 +1,21 @@
+import pytest
+from charitybot2.models.currency import Currency
+from charitybot2.models.event import Event
+
+test_event_currency = Currency(key='USD')
+test_event = Event()
+
+
 class TestEventInstantiation:
-    def retrieve_identifier(self):
-        pass
-
-    def retrieve_title(self):
-        pass
-
-    def retrieve_start_time(self):
-        pass
-
-    def retrieve_end_time(self):
-        pass
-
-    def retrieve_starting_amount(self):
-        pass
-
-    def retrieve_target_amount(self):
-        pass
-
-    def retrieve_update_delay(self):
-        pass
-
-    def retrieve_currency(self):
-        pass
+    @pytest.mark.parametrize('expected,actual', [
+        ('identifier', test_event.identifier),
+        ('title', test_event.title),
+        (0, test_event.start_time),
+        (1, test_event.end_time),
+        (0, test_event.starting_amount),
+        (100, test_event.amount_raised),
+        (5, test_event.update_delay),
+        (test_event_currency.key, test_event.currency.key)
+    ])
+    def test_retrieval(self, expected, actual):
+        assert expected == actual
