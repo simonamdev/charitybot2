5fdc3d94a5148770ba612e8b75a3bc81bd3cf557
Simon AM <simon@agius-muscat.net>
2017-01-29 23:04:09 +0100

    Added basic tests for BaseRepository

--- charitybot2/persistence/base_repository.py
+++ charitybot2/persistence/base_repository.py
@@ -1,2 +1,7 @@
-class BaseRepository:
+class InvalidRepositoryException(Exception):
     pass
+
+
+class BaseRepository:
+    def __init__(self, db_path):
+        self._db_path = db_path
--- tests/unit/test_base_repository.py
+++ tests/unit/test_base_repository.py
@@ -1,6 +1,26 @@
+import pytest
+from charitybot2.persistence.base_repository import BaseRepository, InvalidRepositoryException
+from tests.paths_for_tests import test_repository_db_path, valid_event_config_path
+
+
 class TestBaseRepositoryInstantiation:
-    pass
+    def test_default_debug_is_false(self):
+        base_repository = BaseRepository(db_path=test_repository_db_path)
+        assert base_repository.debug is False
 
 
 class TestBaseRepositoryExceptions:
-    pass
+    @pytest.mark.parametrize('db_path', [
+        '',
+        'Bla',
+        None,
+        123
+    ])
+    def test_passing_incorrect_db_paths_throws_exception(self, db_path):
+        with pytest.raises(FileNotFoundError):
+            BaseRepository(db_path=db_path)
+
+    def test_passing_non_db_path_throws_exception(self):
+        non_db_path = valid_event_config_path
+        with pytest.raises(InvalidRepositoryException):
+            BaseRepository(db_path=non_db_path)
