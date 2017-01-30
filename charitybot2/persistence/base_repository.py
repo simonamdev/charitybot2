774a19a2c1e201eade93780098e71c20f19f7f64
Simon AM <simon@agius-muscat.net>
2017-01-30 13:17:38 +0100

    Added validation for BaseRepository

--- charitybot2/persistence/base_repository.py
+++ charitybot2/persistence/base_repository.py
@@ -1,7 +1,24 @@
+import os
+
+
 class InvalidRepositoryException(Exception):
     pass
 
 
 class BaseRepository:
-    def __init__(self, db_path):
+    def __init__(self, db_path, debug=False):
         self._db_path = db_path
+        self._debug = debug
+        self.__validate_database()
+
+    @property
+    def debug(self):
+        return self._debug
+
+    def __validate_database(self):
+        if not isinstance(self._db_path, str):
+            raise InvalidRepositoryException('Given path is not a string')
+        if '.db' not in self._db_path and '.sqlite' not in self._db_path:
+            raise InvalidRepositoryException('Given path does not lead to a valid database file')
+        if not os.path.isfile(self._db_path):
+            raise FileNotFoundError('Database file not found at given DB Path')
--- tests/unit/test_base_repository.py
+++ tests/unit/test_base_repository.py
@@ -5,22 +5,23 @@ from tests.paths_for_tests import test_repository_db_path, valid_event_config_pa
 
 class TestBaseRepositoryInstantiation:
     def test_default_debug_is_false(self):
+        print(test_repository_db_path)
         base_repository = BaseRepository(db_path=test_repository_db_path)
         assert base_repository.debug is False
 
 
 class TestBaseRepositoryExceptions:
     @pytest.mark.parametrize('db_path', [
-        '',
-        'Bla',
         None,
-        123
+        123,
+        valid_event_config_path,
+        '',
+        'Bla'
     ])
-    def test_passing_incorrect_db_paths_throws_exception(self, db_path):
-        with pytest.raises(FileNotFoundError):
+    def test_passing_invalid_path_throws_exception(self, db_path):
+        with pytest.raises(InvalidRepositoryException):
             BaseRepository(db_path=db_path)
 
-    def test_passing_non_db_path_throws_exception(self):
-        non_db_path = valid_event_config_path
-        with pytest.raises(InvalidRepositoryException):
-            BaseRepository(db_path=non_db_path)
+    def test_passing_incorrect_db_paths_throws_exception(self):
+        with pytest.raises(FileNotFoundError):
+            BaseRepository(db_path='bla/bla.db')
