3a208c8fe06f09a7ff6453bf3e4b5fda6a56df0d
Simon AM <simon@agius-muscat.net>
2017-01-31 18:52:01 +0100

    Added basic outline for Logger

--- charitybot2/creators/logger_creator.py
+++ charitybot2/creators/logger_creator.py
@@ -1,6 +1,7 @@
 """
 Depending on the debug flag, the LoggerCreator will return either a RepositoryLogger or a ConsoleLogger
 """
+from charitybot2.models.log import Log
 from charitybot2.persistence.console_logger import ConsoleLogger
 from charitybot2.persistence.repository_logger import RepositoryLogger
 
@@ -18,3 +19,18 @@ class LoggerCreator:
             return ConsoleLogger()
         else:
             return RepositoryLogger()
+
+
+class Logger:
+    def log(self, log):
+        pass
+
+    def log_verbose(self, timestamp, source, ):
+        verbose_log = self._create_log()
+
+    def log_info(self):
+        pass
+
+    @staticmethod
+    def _create_log(timestamp, level, source, event, message):
+        return Log(timestamp=timestamp, level=level, source=source, event=event, message=message)
