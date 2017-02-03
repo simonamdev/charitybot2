de80060fb83a03e15ebc56af0ee17cf6c5fe0c54
Simon AM <simon@agius-muscat.net>
2017-02-03 21:42:47 +0100

    Fixed basic Event tests

--- charitybot2/models/event.py
+++ charitybot2/models/event.py
@@ -1,2 +1,17 @@
 class Event:
-    pass
+    def __init__(self, configuration):
+        self._configuration = configuration
+        self._starting_amount = 0
+        self._amount_raised = 0
+
+    @property
+    def configuration(self):
+        return self._configuration
+
+    @property
+    def starting_amount(self):
+        return self._starting_amount
+
+    @property
+    def amount_raised(self):
+        return self._amount_raised
--- tests/unit/test_event.py
+++ tests/unit/test_event.py
@@ -1,21 +1,23 @@
 import pytest
-from charitybot2.models.currency import Currency
+from charitybot2.configurations.event_configuration import EventConfiguration
 from charitybot2.models.event import Event
+from tests.unit.test_event_configuration import test_event_configuration_values
 
-test_event_currency = Currency(key='USD')
-test_event = Event()
+test_event_configuration = EventConfiguration(configuration_values=test_event_configuration_values)
+test_event = Event(configuration=test_event_configuration)
 
 
-# class TestEventInstantiation:
-#     @pytest.mark.parametrize('expected,actual', [
-#         ('identifier', test_event.identifier),
-#         ('title', test_event.title),
-#         (0, test_event.start_time),
-#         (1, test_event.end_time),
-#         (0, test_event.starting_amount),
-#         (100, test_event.amount_raised),
-#         (5, test_event.update_delay),
-#         (test_event_currency.key, test_event.currency.key)
-#     ])
-#     def test_retrieval(self, expected, actual):
-#         assert expected == actual
+class TestEventInstantiation:
+    @pytest.mark.parametrize('expected,actual', [
+        (test_event_configuration, test_event.configuration),
+        ('identifier', test_event.configuration.identifier),
+        ('This is a Title', test_event.configuration.title),
+        (0, test_event.configuration.start_time),
+        (1, test_event.configuration.end_time),
+        (5, test_event.configuration.update_delay),
+        (test_event_configuration.currency.key, test_event.configuration.currency.key),
+        (0, test_event.amount_raised),
+        (0, test_event.starting_amount)
+    ])
+    def test_retrieval(self, expected, actual):
+        assert expected == actual
