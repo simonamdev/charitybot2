76a3e157ebf9a47525fa4e700a0df61e44f1c4ab
Simon AM <simon@agius-muscat.net>
2017-02-05 18:33:30 +0100

    Added scraper type recognition

--- charitybot2/sources/mydonate.py
+++ charitybot2/sources/mydonate.py
@@ -1,4 +1,4 @@
-from charitybot2.sources.scraper import Scraper
+from charitybot2.sources.scraper import Scraper, InvalidFundraiserUrlException
 
 
 class MyDonateScraper(Scraper):
@@ -11,4 +11,14 @@ class MyDonateTeamsScraper(MyDonateScraper):
 
 class MyDonateScraperCreator:
     def __init__(self, url, debug=False):
-        pass
+        self.url = url
+        self.debug = debug
+
+    def get_scraper(self):
+        return self.__determine_scraper_type()
+
+    def __determine_scraper_type(self):
+        if 'teams' in self.url:
+            return MyDonateTeamsScraper(url=self.url, debug=self.debug)
+        else:
+            raise InvalidFundraiserUrlException('Unable to figure out fundraiser type from URL')
