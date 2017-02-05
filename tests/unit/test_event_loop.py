e8ec187bf303a9a319b81f813ffd640ca8225a45
Simon AM <simon@agius-muscat.net>
2017-02-05 19:27:55 +0100

    Adjusted tests to take mydonate into consideration

--- tests/unit/test_event_loop.py
+++ tests/unit/test_event_loop.py
@@ -5,15 +5,12 @@ from charitybot2.botconfig.event_config import EventConfigurationFromFile, Event
 from charitybot2.events.event_loop import EventLoop
 from charitybot2.events.event import Event, EventInvalidException
 from charitybot2.sources.justgiving import JustGivingScraper
+from charitybot2.sources.mocks.mocksite import mock_justgiving_fundraising_url, mock_mydonate_teams_url
+from charitybot2.sources.mydonate import MyDonateScraper
 from tests.paths_for_tests import valid_config_path, repository_db_path, repository_db_script_path
 from tests.mocks import ResetDB
 
 valid_event_config_values = EventConfigurationFromFile(file_path=valid_config_path).get_config_data()
-valid_event_configuration = EventConfigurationCreator(config_values=valid_event_config_values).get_event_configuration()
-
-btdonate_config_values = copy.deepcopy(valid_event_config_values)
-btdonate_config_values['source_url'] = 'https://mydonate.bt.com/fundraisers/acpi'
-btdonate_configuration = EventConfigurationCreator(config_values=btdonate_config_values).get_event_configuration()
 
 
 def get_updated_configuration(updates=None):
@@ -28,8 +25,8 @@ def setup_module():
 
 
 class ValidTestEvent(Event):
-    def __init__(self):
-        super().__init__(event_configuration=valid_event_configuration, db_path=repository_db_path)
+    def __init__(self, event_configuration=get_updated_configuration()):
+        super().__init__(event_configuration=event_configuration, db_path=repository_db_path)
 
 
 class TestEventLoopValidity:
@@ -40,14 +37,13 @@ class TestEventLoopValidity:
     def test_initialise_with_valid_event(self):
             el = EventLoop(event=ValidTestEvent(), debug=True)
 
-    def test_initialise_not_implemented_btdonate_scraper_throws_exception(self):
-        with pytest.raises(NotImplementedError):
-            e = Event(event_configuration=btdonate_configuration, db_path=repository_db_path)
-            el = EventLoop(event=e, debug=True)
-
-    def test_valid_event_loop_scraper_is_of_type_justgivingscraper(self):
-        el = EventLoop(event=ValidTestEvent(), debug=True)
-        assert isinstance(el.scraper, JustGivingScraper)
+    @pytest.mark.parametrize('event_configuration,expected_type', [
+        (get_updated_configuration({'source_url': mock_justgiving_fundraising_url}), JustGivingScraper),
+        (get_updated_configuration({'source_url': mock_mydonate_teams_url}), MyDonateScraper)
+    ])
+    def test_scraper_types_according_to_url(self, event_configuration, expected_type):
+        el = EventLoop(event=ValidTestEvent(event_configuration=event_configuration), debug=True)
+        assert isinstance(el.scraper, expected_type)
 
     def test_current_amount_is_equal_to_starting_amount_when_no_donations_present(self):
         no_donations_config = get_updated_configuration({'internal_name': 'NoDonations'})
