780df92fae156673e99736bc037abc2ba7d6cfe4
Simon AM <simon@agius-muscat.net>
2017-02-06 23:20:15 +0100

    Privatised Event Registration check

--- charitybot2/creators/event_creator.py
+++ charitybot2/creators/event_creator.py
@@ -1,5 +1,8 @@
+import time
 from charitybot2.configurations.event_configuration import EventConfiguration
 from charitybot2.creators.event_configuration_creator import InvalidEventConfigurationException
+from charitybot2.persistence.event_repository import EventRepository
+from charitybot2.persistence.logger import Logger
 
 
 class EventCreator:
@@ -7,17 +10,32 @@ class EventCreator:
         self._event_configuration = event_configuration
         self._debug = debug
         self.__validate_event_configuration()
+        self._logger = Logger(source='EventCreator', event=self._event_configuration.identifier)
 
     @property
     def debug(self):
         return self._debug
 
+    def get_event(self):
+        pass
+
     def __validate_event_configuration(self):
         if not isinstance(self._event_configuration, EventConfiguration):
             raise InvalidEventConfigurationException('Event creator can only accept Event Configurations')
 
-    def get_event(self):
+    def _register_event(self):
         pass
 
-    def event_is_registered(self, event_identifier):
+    def _update_event(self):
         pass
+
+    def __event_is_registered(self, event_identifier):
+        event_repository = EventRepository(debug=self.debug)
+        event_repository.open_connection()
+        registered = event_repository.event_already_registered(identifier=event_identifier)
+        event_repository.close_connection()
+        if registered:
+            self._logger.log_info(timestamp=int(time.time()), message='Event is already registered')
+        else:
+            self._logger.log_info(timestamp=int(time.time()), message='Event is not registered yet')
+        return registered
--- tests/unit/test_event_creator.py
+++ tests/unit/test_event_creator.py
@@ -5,8 +5,6 @@ from charitybot2.creators.event_creator import EventCreator
 from charitybot2.models.event import Event
 from tests.unit.test_event_configuration_creator import get_updated_test_config_values
 
-test_event_configuration = get_updated_test_config_values()
-
 
 def get_test_configuration(updated_values=None):
     test_event_configuration_creator = EventConfigurationCreator(
@@ -24,22 +22,18 @@ class TestEventCreation:
     def test_creating_unregistered_event(self):
         registration_test_configuration = get_test_configuration({'identifier': 'registration_test'})
         event_creator = EventCreator(event_configuration=registration_test_configuration)
-        assert event_creator.event_is_registered(event_identifier=registration_test_configuration.identifier) is False
         new_event = event_creator.get_event()
         assert isinstance(new_event, Event)
         assert new_event.configuration.identifier == registration_test_configuration.identifier
-        assert event_creator.event_is_registered(event_identifier=registration_test_configuration.identifier) is True
 
     def test_updating_registered_event(self):
         update_test_configuration = get_test_configuration({'identifier': 'update_event_test'})
         event_creator = EventCreator(event_configuration=update_test_configuration)
         test_event = event_creator.get_event()
         assert isinstance(test_event, Event)
-        assert event_creator.event_is_registered(test_event) is True
         update_test_configuration = get_test_configuration({'end_time': 999})
         event_creator = EventCreator(event_configuration=update_test_configuration)
         test_event = event_creator.get_event()
-        assert event_creator.event_is_registered(test_event) is True
         assert test_event.configuration.end_time == update_test_configuration.end_time
 
 
