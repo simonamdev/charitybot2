0e82ce6a2f49edebf330af40122ef8b1c41d71a2
Simon AM <simon@agius-muscat.net>
2017-02-06 23:22:28 +0100

    Added event creation logic

--- charitybot2/creators/event_creator.py
+++ charitybot2/creators/event_creator.py
@@ -1,26 +1,31 @@
 import time
 from charitybot2.configurations.event_configuration import EventConfiguration
 from charitybot2.creators.event_configuration_creator import InvalidEventConfigurationException
+from charitybot2.models.event import Event
 from charitybot2.persistence.event_repository import EventRepository
 from charitybot2.persistence.logger import Logger
 
 
 class EventCreator:
     def __init__(self, event_configuration, debug=False):
-        self._event_configuration = event_configuration
+        self._configuration = event_configuration
         self._debug = debug
         self.__validate_event_configuration()
-        self._logger = Logger(source='EventCreator', event=self._event_configuration.identifier)
+        self._logger = Logger(source='EventCreator', event=self._configuration.identifier)
 
     @property
     def debug(self):
         return self._debug
 
     def get_event(self):
-        pass
+        if self.__event_is_registered():
+            self._update_event()
+        else:
+            self._register_event()
+        return Event(configuration=self._configuration)
 
     def __validate_event_configuration(self):
-        if not isinstance(self._event_configuration, EventConfiguration):
+        if not isinstance(self._configuration, EventConfiguration):
             raise InvalidEventConfigurationException('Event creator can only accept Event Configurations')
 
     def _register_event(self):
@@ -29,10 +34,10 @@ class EventCreator:
     def _update_event(self):
         pass
 
-    def __event_is_registered(self, event_identifier):
+    def __event_is_registered(self):
         event_repository = EventRepository(debug=self.debug)
         event_repository.open_connection()
-        registered = event_repository.event_already_registered(identifier=event_identifier)
+        registered = event_repository.event_already_registered(identifier=self._configuration.identifier)
         event_repository.close_connection()
         if registered:
             self._logger.log_info(timestamp=int(time.time()), message='Event is already registered')
