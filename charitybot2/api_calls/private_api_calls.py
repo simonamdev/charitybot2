6cb266192fb99efdc64c030894872c7c06b85c31
Simon AM <simon@agius-muscat.net>
2017-04-16 11:36:06 +0200

    Added type safety to private API calls

--- charitybot2/api_calls/private_api_calls.py
+++ charitybot2/api_calls/private_api_calls.py
@@ -2,11 +2,11 @@ import json
 
 import time
 
-from charitybot2.exceptions import IllegalArgumentException
+from charitybot2.configurations.event_configuration import EventConfiguration
 from charitybot2.models.donation import Donation
 from charitybot2.private_api.private_api import private_api_full_url
 from charitybot2.sources.url_call import UrlCall
-from type_assertions import assert_type, assert_types
+from type_assertions import assert_types, accept_types
 
 
 class PrivateApiCalls:
@@ -18,36 +18,35 @@ class PrivateApiCalls:
     def get_index(self):
         return json.loads(UrlCall(url=self.v1_url, timeout=self._timeout).get().content.decode('utf-8'))
 
+    @accept_types(object, str)
     def get_event_existence(self, identifier):
         url = self.v1_url + 'event/exists/{}/'.format(identifier)
         decoded_content = UrlCall(url=url, timeout=self._timeout).get().content.decode('utf-8')
         return json.loads(decoded_content)['event_exists']
 
+    @accept_types(object, str)
     def get_event_info(self, identifier):
         url = self.v1_url + 'event/{}'.format(identifier)
         decoded_content = UrlCall(url=url, timeout=self._timeout).get().content.decode('utf-8')
         content = json.loads(decoded_content)
         return content if len(content.keys()) > 0 else None
 
+    @accept_types(object, EventConfiguration)
     def register_event(self, event_configuration):
         url = self.v1_url + 'event/'
         response = UrlCall(url=url, timeout=self._timeout).post(data=event_configuration.configuration_values)
         decoded_content = response.content.decode('utf-8')
         return json.loads(decoded_content)['registration_successful']
 
+    @accept_types(object, EventConfiguration)
     def update_event(self, event_configuration):
         url = self.v1_url + 'event/'
         response = UrlCall(url=url, timeout=self._timeout).post(data=event_configuration.configuration_values)
         decoded_content = response.content.decode('utf-8')
         return json.loads(decoded_content)['update_successful']
 
+    @accept_types(object, str, str, (int, float))
     def send_heartbeat(self, source, state, timestamp=None):
-        assert_types(
-            (
-                (source, str, "Source can only be a String"),
-                (state, str, "State can only be of type string")
-            )
-        )
         if timestamp is None or not isinstance(timestamp, int):
             timestamp = int(time.time())
         data = dict(state=state, source=source, timestamp=timestamp)
@@ -56,7 +55,7 @@ class PrivateApiCalls:
         decoded_content = response.content.decode('utf-8')
         return json.loads(decoded_content)['received']
 
+    @accept_types(Donation)
     def register_donation(self, donation):
-        assert_type(donation, Donation, "Only Donation type accepted for donation registration")
         # TODO: POST request to register donation for a given event
         pass
