e1835fc37febbc114ed893e846acb0dc6498d27e
Simon AM <simon@agius-muscat.net>
2017-04-16 10:21:51 +0200

    Added decorator to expect types

--- tests/unit/test_type_assertions.py
+++ tests/unit/test_type_assertions.py
@@ -1,6 +1,6 @@
 import pytest
 from charitybot2.exceptions import IllegalArgumentException
-from type_assertions import assert_types, assert_type
+from type_assertions import assert_types, assert_type, accept_types
 
 integer = 5
 real = 0.5
@@ -36,3 +36,22 @@ class TestTypeAssertion:
     ])
     def test_passing_valid_parameters(self, variable, type, message):
         assert_type(variable, type, message)
+
+    def test_wrapping_function_with_type_assertion(self):
+        @accept_types(str)
+        def wrap_test(this_should_be_string):
+            print(this_should_be_string)
+        wrap_test('bla')
+
+    def test_wrapping_function_returns_function_if_types_are_correct(self):
+        @accept_types(int)
+        def multiply_by_5(number):
+            return number * 5
+        assert 25 == multiply_by_5(5)
+
+    def test_receiving_incorrect_expected_type_throws_exception(self):
+        @accept_types(str, int)
+        def two_strings(one, two):
+            pass
+        with pytest.raises(IllegalArgumentException):
+            two_strings('Actually a string', 'Also a string')
