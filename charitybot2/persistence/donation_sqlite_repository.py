5c8259fd39d5500601e3ca214d36b9b5d51e4e91
Simon AM <simon@agius-muscat.net>
2017-04-16 21:34:16 +0200

    Added basic tests for Donation Repository

--- charitybot2/persistence/donation_sqlite_repository.py
+++ charitybot2/persistence/donation_sqlite_repository.py
@@ -1,6 +1,19 @@
 from charitybot2.persistence.sqlite_repository import SQLiteRepository
 
 
+class DonationAlreadyRegisteredException(Exception):
+    pass
+
+
 class DonationSQLiteRepository(SQLiteRepository):
     def __init__(self, db_path='memory', debug=False):
         super().__init__(db_path=db_path, debug=debug)
+
+    def record_donation(self, donation):
+        pass
+
+    def get_event_donations(self, event_identifier):
+        pass
+
+    def get_latest_event_donation(self, event_identifier):
+        pass
--- tests/integration/test_donation_sqlite_repository.py
+++ tests/integration/test_donation_sqlite_repository.py
@@ -1,4 +1,16 @@
-from charitybot2.persistence.donation_sqlite_repository import DonationSQLiteRepository
+import pytest
+from charitybot2.exceptions import IllegalArgumentException
+from charitybot2.models.donation import Donation
+from charitybot2.persistence.donation_sqlite_repository import DonationSQLiteRepository, \
+    DonationAlreadyRegisteredException
+
+
+def setup_test_donations(repository):
+    donations = []
+    for i in range(1, 6):
+        donations.append(Donation(amount=i, event_identifier='event', timestamp=i))
+    for donation in donations:
+        repository.record_donation(donation=donation)
 
 
 class TestDonationSQLiteRepository:
@@ -6,11 +18,33 @@ class TestDonationSQLiteRepository:
 
     def setup_method(self):
         self.test_donation_repository = DonationSQLiteRepository(debug=True)
+        setup_test_donations(self.test_donation_repository)
 
     def teardown_method(self):
         self.test_donation_repository.close_connection()
 
+    def test_getting_all_donations(self):
+        values = range(1, 6)
+        donations = self.test_donation_repository.get_event_donations(event_identifier='event')
+        for donation in donations:
+            assert donation.amount in values
+            assert donation.timestamp in values
+            assert donation.event_identifier == 'event'
 
+    def test_getting_latest_donation(self):
+        latest_donation = self.test_donation_repository.get_latest_event_donation(event_identifier='event')
+        assert 5 == latest_donation.amount
+        assert 5 == latest_donation.timestamp
+        assert 'event' == latest_donation.event_identifier
+
+    def test_recording_donation(self):
+        new_donation = Donation(amount=420, event_identifier='420', timestamp=420, identifier='wololo')
+        self.test_donation_repository.record_donation(donation=new_donation)
+        latest_donation = self.test_donation_repository.get_latest_event_donation(event_identifier='420')
+        assert new_donation.amount == latest_donation.amount
+        assert new_donation.timestamp == latest_donation.timestamp
+        assert new_donation.event_identifier == latest_donation.event_identifier
+        assert new_donation.identifier == latest_donation.identifier
 
 
 class TestDonationSQLiteRepositoryExceptions:
@@ -18,6 +52,27 @@ class TestDonationSQLiteRepositoryExceptions:
 
     def setup_method(self):
         self.test_donation_repository = DonationSQLiteRepository(debug=True)
+        setup_test_donations(self.test_donation_repository)
 
     def teardown_method(self):
         self.test_donation_repository.close_connection()
+
+    def test_recording_already_recorded_donation(self):
+        donation = Donation(amount=500, event_identifier='wooo', timestamp=500)
+        self.test_donation_repository.record_donation(donation=donation)
+        with pytest.raises(DonationAlreadyRegisteredException):
+            self.test_donation_repository.record_donation(donation=donation)
+
+    @pytest.mark.parametrize('donation', [
+        object,
+        '',
+        'bla',
+        123,
+        1.23,
+        None,
+        [],
+        ()
+    ])
+    def test_passing_incorrect_values_throws_exception(self, donation):
+        with pytest.raises(IllegalArgumentException):
+            self.test_donation_repository.record_donation(donation=donation)
