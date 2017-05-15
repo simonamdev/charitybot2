c0fcf24fc25b09fcf63cba982d72574a6550e6a6
Simon AM <simon@agius-muscat.net>
2017-05-15 15:08:35 +0200

    Display donations in table

--- charitybot2/public_api/console/static/js/event-ui.js
+++ charitybot2/public_api/console/static/js/event-ui.js
@@ -1,5 +1,6 @@
-apiUrl = 'http://127.0.0.1:8001/api/v1/';
-debugMode = true;
+var apiUrl = 'http://127.0.0.1:8001/api/v1/';
+var debugMode = true;
+var currencyKey = "";
 
 eventUI();
 
@@ -18,10 +19,15 @@ function getEventDetails(eventIdentifier) {
     console.log('Retrieving data for event: ' + eventIdentifier);
     var url = apiUrl + 'event/' + eventIdentifier;
     $.getJSON(url, data => {
+        setupCurrencyKey(data);
         drawEventDetails(data);
     }).fail(() => {
         console.error('Could not reach URL: ' + url);
     });
+}
+
+function setupCurrencyKey(data) {
+    currencyKey = data['currency_key'];
 }
 
 function drawEventDetails(data) {
@@ -41,4 +47,19 @@ function getDonations(eventIdentifier, lowerTimeBound, upperTimeBound) {
 
 function drawDonations(data) {
     debugPrint(data);
+    var donationsData = data['donations'];
+    var donationsTable = $('#donations-table');
+    for (i = 0; i < donationsData.length; i++) {
+        var rowDonation = JSON.parse(donationsData[i]);
+        var rowString = '<tr><td>' +
+                        rowDonation['identifier'] +
+                        '</td><td>' +
+                        rowDonation['amount'] +
+                        '</td><td>' +
+                        rowDonation['timestamp'] +
+                        '</td><td>' +
+                        rowDonation['notes']
+                        '</td></tr>'
+        $('#donations-table').append(rowString);
+    }
 }
