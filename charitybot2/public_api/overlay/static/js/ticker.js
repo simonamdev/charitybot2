afe1d67070dc64bcd56e434af9ac517f038662e5
Simon AM <simon@agius-muscat.net>
2017-05-27 16:49:17 +0200

    Updated ticker to use new currency methods

--- charitybot2/public_api/overlay/static/js/ticker.js
+++ charitybot2/public_api/overlay/static/js/ticker.js
@@ -2,9 +2,26 @@ var apiUrl = 'http://127.0.0.1:8001/api/v1/';
 var eventUrl = apiUrl + 'event/' + eventIdentifier;
 var donationsUrl = eventUrl + '/donations/';
 
-updateTicker(numberOfDonations);
+var currencySymbol = 'N/A';
+var updateDelay = 2000; // ms
+
+
+// Get the currency symbol then setup the updating cycle
+getCurrencySymbol().then((symbol) => {
+    currencySymbol = symbol;
+    updateTicker(numberOfDonations);
+    setInterval(
+        () => {
+            updateTicker(numberOfDonations);
+        },
+        updateDelay
+    );
+}).catch((error) => {
+    console.error(error);
+});
 
 function updateTicker(limit) {
+    console.log('Updating ticker');
     fetchJSONFile(
         donationsUrl + '?limit=' + limit,
         (response) => {
@@ -12,7 +29,8 @@ function updateTicker(limit) {
         },
         () => {
             console.error('Unable to connect to retrieve donations');
-        });
+        }
+    );
 }
 
 function drawTableRows(data) {
@@ -23,7 +41,7 @@ function drawTableRows(data) {
     var tableRows = '';
     for (var i = 0; i < data.donations.length; i++) {
         var donation = JSON.parse(data.donations[i]);
-        var tableRow = '<tr><td>' + donation['timestamp'] + '</td><td>' + donation['amount'] + '</td></tr>';
+        var tableRow = '<tr><td>' + donation['timestamp'] + '</td><td>' + currencySymbol + donation['amount'] + '</td></tr>';
         tableRows += tableRow;
     }
     table.innerHTML += tableRows;
