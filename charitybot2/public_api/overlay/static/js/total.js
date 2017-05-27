84883f7460dee286402574018006695ac78e7542
Simon AM <simon@agius-muscat.net>
2017-05-27 16:39:24 +0200

    Converted currency to promises

--- charitybot2/public_api/overlay/static/js/total.js
+++ charitybot2/public_api/overlay/static/js/total.js
@@ -2,29 +2,6 @@ var apiUrl = 'http://127.0.0.1:8001/api/v1/';
 var eventUrl = apiUrl + 'event/' + eventIdentifier;
 var totalUrl = eventUrl + '/total/';
 
-function getCurrencySymbol(currencyKey) {
-    if (currencyKey == 'EUR') {
-        return '€';
-    } else if (currencyKey == 'USD') {
-        return '$';
-    }
-    return '£';
-}
-
-function getCurrency() {
-    fetchJSONFile(
-        eventUrl,
-        (data) => {
-            console.log(data);
-            var header = document.getElementById('overlay-currency');
-            header.innerHTML = getCurrencySymbol(data['currency_key']);
-        },
-        () => {
-            console.error('Unable to connect to retrieve currency');
-        }
-    );
-}
-
 function getTotal() {
     console.log('Getting Total');
     fetchJSONFile(totalUrl, (data) => {
@@ -41,7 +18,7 @@ function markUnavailable() {
     header.innerHTML = 'Unable to connect';
 }
 
-getCurrency();
+drawCurrencySymbolOnPage();
 getTotal();
 var updateDelay = 2000; // ms
 setInterval(getTotal, updateDelay)
