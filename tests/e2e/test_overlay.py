c4c12e2187dc0611331c34263d4454a2111aa0d0
Simon AM <simon@agius-muscat.net>
2017-06-18 08:16:29 +0200

    Fixed unmerged overlay test

--- tests/e2e/test_overlay.py
+++ tests/e2e/test_overlay.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import time
-
-import pytest
-import requests
-
-from bs4 import BeautifulSoup
-from charitybot2.botconfig.event_config import EventConfigurationFromFile
-from charitybot2.events.event_loop import EventLoop
-from charitybot2.reporter.external_api.external_api import api_full_url
-from charitybot2.sources.mocks.mocksite import mock_justgiving_fundraising_url, mock_mydonate_teams_url, \
-    mock_justgiving_api_url
-=======
 import random
 from time import sleep
 
@@ -21,11 +8,9 @@ from charitybot2.paths import overlay_script_path, private_api_script_path
 from charitybot2.private_api.private_api import private_api_service
 from charitybot2.public_api.overlay.overlay import app
 from charitybot2.start_service import Service, ServiceRunner
->>>>>>> refs/remotes/origin/lavecon-ui
 from selenium import webdriver
 from tests.integration.test_event_register import get_test_event_configuration
 from tests.setup_test_database import setup_test_database
-from datetime import datetime
 
 driver = None
 test_event_identifier = get_test_event_configuration().identifier
@@ -55,27 +40,6 @@ api_service_runner = ServiceRunner(
     start_delay=2,
     stop_delay=1)
 
-<<<<<<< HEAD
-def get_new_mock_event(source_url, end_time_increment=10):
-    end_time = int(time.time()) + end_time_increment
-    config_adjustment.change_value(key='end_time', value=end_time)
-    event_configuration = EventConfigurationFromFile(file_path=end_to_end_config_path).get_event_configuration()
-    return MockEvent(
-        config_path=end_to_end_config_path,
-        mock_name=event_configuration.get_internal_name(),
-        mock_end_time=end_time,
-        source_url=source_url)
-
-mock_justgiving_fundraising_website = MockFundraisingWebsite(fundraiser_name='justgiving')
-mock_mydonate_fundraising_website = MockFundraisingWebsite(fundraiser_name='mydonate')
-
-mock_external_api = MockExternalAPI(extra_args=['--debug'], enter_debug=True)
-
-
-def setup_module():
-    mock_justgiving_fundraising_website.start()
-    mock_external_api.start()
-=======
 
 def get_soup_text_by_id(tag_id):
     global driver
@@ -85,67 +49,18 @@ def get_soup_text_by_id(tag_id):
 def setup_module():
     overlay_service_runner.run()
     api_service_runner.run()
->>>>>>> refs/remotes/origin/lavecon-ui
     global driver
     driver = webdriver.Chrome()
     driver.implicitly_wait(10)
 
 
 def teardown_module():
-<<<<<<< HEAD
-    mock_justgiving_fundraising_website.stop()
-    mock_external_api.stop()
-=======
     overlay_service_runner.stop_running()
     api_service_runner.stop_running()
->>>>>>> refs/remotes/origin/lavecon-ui
     global driver
     driver.close()
 
-end_to_end_test_params = [
-    (mock_justgiving_fundraising_url, mock_justgiving_fundraising_website),
-    (mock_justgiving_api_url,         mock_justgiving_fundraising_website),
-    (mock_mydonate_teams_url,         mock_mydonate_fundraising_website)
-]
 
-
-<<<<<<< HEAD
-class TestOverlay:
-    overlay_url = api_full_url + 'overlay/{}'
-
-    @pytest.mark.parametrize('source_url,mocksite', end_to_end_test_params)
-    def test_getting_last_donation_amount_on_overlay(self, source_url, mocksite):
-        ResetDB(db_path=repository_db_path, sql_path=repository_db_script_path)
-        mock_event = get_new_mock_event(source_url=source_url)
-        mocksite.reset_amount()
-        EventLoop(event=mock_event, debug=True).start()
-        response = requests.get(url=self.overlay_url.format(mock_event.get_internal_name()))
-        assert 200 == response.status_code
-        assert '<!DOCTYPE html>' in response.content.decode('utf-8')
-        soup = BeautifulSoup(response.content, 'html.parser')
-        amount_raised = soup.find('span', {'id': 'amount_raised'}).text.strip()
-        assert '100' == amount_raised
-        overlay_text = soup.find('div', {'id': 'overlay-text'}).text.strip().replace('\n', '')
-        assert '€100' == overlay_text
-
-    @pytest.mark.parametrize('source_url,mocksite', end_to_end_test_params)
-    def test_overlay_amount_updates_automagically(self, source_url, mocksite):
-        ResetDB(db_path=repository_db_path, sql_path=repository_db_script_path)
-        mock_event = get_new_mock_event(source_url=source_url)
-        mocksite.reset_amount()
-        driver.get(self.overlay_url.format(mock_event.get_internal_name()))
-        EventLoop(event=mock_event, debug=True).start()
-        soup = BeautifulSoup(driver.find_element_by_id('amount_raised').text.strip(), 'html.parser')
-        assert '100' == soup.text
-        soup = BeautifulSoup(driver.find_element_by_id('overlay-text').text.strip(), 'html.parser')
-        assert '€100' == soup.text
-        mocksite.increase_amount()
-        EventLoop(event=get_new_mock_event(source_url=source_url), debug=True).start()
-        soup = BeautifulSoup(driver.find_element_by_id('amount_raised').text.strip(), 'html.parser')
-        assert '150' == soup.text
-        soup = BeautifulSoup(driver.find_element_by_id('overlay-text').text.strip(), 'html.parser')
-        assert '€150' == soup.text
-=======
 class TestOverlayTotal:
     overlay_total_url = overlay_service.full_url + 'overlay/{}/total'.format(test_event_identifier)
 
@@ -267,4 +182,3 @@ class TestOverlayLatestDonation:
         assert test_amount == amount
         assert 'donor' == donor.replace(',', '')
         # assert test_amount == int(timestamp)
->>>>>>> refs/remotes/origin/lavecon-ui
