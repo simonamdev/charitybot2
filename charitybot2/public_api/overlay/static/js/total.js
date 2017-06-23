b8cd11d27711547fa60c0c526ac302cd9d7a8c7e
Simon AM <simon@agius-muscat.net>
2017-06-23 13:04:07 +0200

    Removed reliance on passed debug variable

--- charitybot2/public_api/overlay/static/js/total.js
+++ charitybot2/public_api/overlay/static/js/total.js
@@ -1,15 +1,10 @@
-//var apiUrl = 'https://api.charitybot.net/api/v1/';
 apiAddress = apiAddress + '/api/v1';
 var eventUrl = apiAddress + '/event/' + eventIdentifier;
 var totalUrl = eventUrl + '/total/';
 
 console.log('Connecting to API via: ' + apiAddress);
-if (!updateDelay) {
-    var updateDelay = 10000; // ms
-}
 
 function getTotal() {
-    console.log(totalUrl);
     fetchJSONFile(totalUrl, (data) => {
         console.log(data);
         var header = document.getElementById('overlay-amount');
@@ -21,7 +16,7 @@ function getTotal() {
 
 function markUnavailable() {
     var header = document.getElementById('overlay-amount');
-    header.innerHTML = 'Unable to connect';
+    header.innerHTML = 'Connection error';
 }
 
 drawCurrencySymbolOnPage();
