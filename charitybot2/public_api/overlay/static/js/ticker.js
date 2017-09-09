6ae3c97016924df3d5f2ee88eaf4021b36f0557f
Simon AM <simon@agius-muscat.net>
2017-09-09 13:33:28 +0200

    Removed table layout from ticker

--- charitybot2/public_api/overlay/static/js/ticker.js
+++ charitybot2/public_api/overlay/static/js/ticker.js
@@ -25,7 +25,7 @@ function updateTicker(limit) {
     fetchJSONFile(
         donationsUrl + '?limit=' + limit,
         (response) => {
-            drawTableRows(response);
+            drawTableRows(response.donations);
         },
         () => {
             console.error('Unable to connect to retrieve donations');
@@ -33,16 +33,37 @@ function updateTicker(limit) {
     );
 }
 
-function drawTableRows(data) {
-    var table = document.getElementById('ticker-table');
-    var tableHeader = '<tr><th>Latest Donations</th></tr>';
-    table.innerHTML = '';
-    table.innerHTML = tableHeader;
-    var tableRows = '';
-    for (var i = 0; i < data.donations.length; i++) {
-        var donation = JSON.parse(data.donations[i]);
-        var donationRow = '<tr><td>' + currencySymbol + donation['amount'] + ', ' + returnTimespanString(((new Date()).getTime() / 1000) - donation['timestamp']) + ' ago</td></tr>';
-        tableRows += donationRow;
+function drawTableRows(donations) {
+    var wrapperEl = document.getElementById('ticker-wrapper');
+    // Clear out the old HTML first TODO: Remove this system
+    wrapperEl.innerHTML = '';
+    for (var i = 0; i < donations.length; i++) {
+        var donation = JSON.parse(donations[i]);
+        console.log(donation);
+        var rowEl = createRowEl(donation['amount'], donation['donor_name'], donation['timestamp']);
+
+        rowEl.classList.add('row');
+        wrapperEl.appendChild(rowEl);
     }
-    table.innerHTML += tableRows;
+}
+
+function createRowEl(amount, name, timestamp) {
+    var parentEl = document.createElement('div');
+    var nameEl = document.createElement('p');
+    nameEl.classList.add('name');
+    var amountEl = document.createElement('p');
+    amountEl.classList.add('amount');
+    var timestampEl = document.createElement('p');
+    timestampEl.classList.add('timestamp');
+    nameEl.innerText = 'Donation by ' + name + ' of ';
+    // If no name is provided, override it
+    if (!name) {
+        nameEl.innerText = 'Donation of ';
+    }
+    amountEl.innerText = currencySymbol + amount;
+    timestampEl.innerText = ' ' + returnTimespanString(((new Date()).getTime() / 1000) - timestamp) + ' ago';
+    parentEl.appendChild(nameEl);
+    parentEl.appendChild(amountEl);
+    parentEl.appendChild(timestampEl);
+    return parentEl;
 }
