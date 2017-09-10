bc157f7c266cc50c9d3e4a9d9c4cef5f8ef3b996
Simon AM <simon@agius-muscat.net>
2017-09-10 20:51:05 +0200

    Added continuous animation to ticker

--- charitybot2/public_api/overlay/static/js/ticker.js
+++ charitybot2/public_api/overlay/static/js/ticker.js
@@ -4,6 +4,7 @@ var eventUrl = apiAddress + '/event/' + eventIdentifier;
 var donationsUrl = eventUrl + '/donations/';
 
 var currencySymbol = 'N/A';
+var currentDonations = [];
 
 console.log('Connecting to API via: ' + apiAddress);
 
@@ -25,7 +26,8 @@ function updateTicker(limit) {
     fetchJSONFile(
         donationsUrl + '?limit=' + limit,
         (response) => {
-            drawTableRows(response.donations);
+            var parsedDonationData = parseDonations(response.donations);
+            renderCurrentDonations(parsedDonationData);
         },
         () => {
             console.error('Unable to connect to retrieve donations');
@@ -33,26 +35,89 @@ function updateTicker(limit) {
     );
 }
 
-function drawTableRows(donations) {
-    var wrapperEl = document.getElementById('ticker-wrapper');
-    // Clear out the old HTML first TODO: Remove this system
-    wrapperEl.innerHTML = '';
-    for (var i = 0; i < donations.length; i++) {
-        var donation = JSON.parse(donations[i]);
-        var rowEl = createRowEl(donation['amount'], donation['donor_name'], donation['timestamp']);
+function parseDonations(donationsFromApi) {
+    // Get new donations to keep
+    var donationsToKeep = donationsFromApi.filter((donation) => {
+        if (currentDonations.indexOf(donation) !== -1) {
+            return donation;
+        }
+    });
+    // Get donations to add
+    var donationsToAdd = donationsFromApi.filter((donation) => {
+        if (currentDonations.indexOf(donation) === -1) {
+            return donation;
+        }
+    });
+    // Get donations to remove
+    var donationsToRemove = currentDonations.filter((donation) => {
+        if (donationsFromApi.indexOf(donation) === -1) {
+            return donation;
+        }
+    });
+//    console.log(donationsToKeep);
+//    console.log(donationsToAdd);
+//    console.log(donationsToRemove);
+    currentDonations = donationsToKeep;
+    currentDonations = currentDonations.concat(donationsToAdd);
+    return {
+        'keep': donationsToKeep,
+        'add': donationsToAdd,
+        'remove': donationsToRemove
+    };
+}
 
+function renderCurrentDonations(parsedDonationData) {
+    var donationsToAdd = parsedDonationData['add'];
+    var donationsToRemove = parsedDonationData['remove'];
+    var wrapperEl = document.getElementById('ticker-wrapper');
+    // Hide donations to be removed
+    for (var i = 0; i < donationsToRemove.length; i++) {
+        let donation = JSON.parse(donationsToRemove[i]);
+        // console.log(donation);
+        let relevantTextEl = document.getElementById(donation['internal_reference']);
+        // TODO: Instead of hiding, add a css class which animates it out, then remove it
+        let rowToRemoveEl = relevantTextEl.parentNode;
+        // console.log(rowToRemoveEl);
+        setTimeout(() => {
+            rowToRemoveEl.classList.add('zoomOut');
+            setTimeout(() => {
+                rowToRemoveEl.parentNode.removeChild(rowToRemoveEl);
+            }, 100);
+        }, 500);
+    }
+    // Add new donations
+    for (var i = donationsToAdd.length - 1; i >= 0; i--) {
+        let donation = JSON.parse(donationsToAdd[i]);
+        // console.log(donation);
+        let rowEl = createRowEl(
+            donation['amount'],
+            donation['donor_name'],
+            donation['timestamp'],
+            donation['internal_reference']
+        );
         rowEl.classList.add('row');
-        wrapperEl.appendChild(rowEl);
+        rowEl.classList.add('animated');
+        rowEl.classList.add('zoomInUp');
+        // Add the first set instantly
+        if (donationsToRemove.length == 0) {
+            wrapperEl.insertBefore(rowEl, wrapperEl.firstChild);
+        } else {
+            setTimeout(() => {
+                wrapperEl.insertBefore(rowEl, wrapperEl.firstChild);
+            }, 500);
+        }
     }
 }
 
-function createRowEl(amount, name, timestamp) {
+function createRowEl(amount, name, timestamp, id) {
     var parentEl = document.createElement('div');
     var textEl = document.createElement('p');
+    // Setting the ID lets us remove it from the list later
+    textEl.id = id;
     textEl.classList.add('text');
     textEl.classList.add('fulljustify');
     name = name || 'Anonymous';
-    var rowString = currencySymbol + amount + ' from ' + name + ', ' + returnTimespanString(((new Date()).getTime() / 1000) - timestamp) + ' ago';;
+    var rowString = currencySymbol + amount + ' from ' + name;
     textEl.innerText = rowString;
     parentEl.appendChild(textEl);
     return parentEl;
