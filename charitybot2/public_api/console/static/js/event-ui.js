8718fb2c910dd17b94cb88cd6b53e25ef8b1b73e
Simon AM <simon@agius-muscat.net>
2017-09-21 19:28:48 +0200

    Removed redundant parts

--- charitybot2/public_api/console/static/js/event-ui.js
+++ charitybot2/public_api/console/static/js/event-ui.js
@@ -5,7 +5,6 @@ var eventExistenceUrl = apiAddress + '/event/exists/' + eventIdentifier;
 var eventTotalUrl = eventUrl + '/total/';
 var donationsUrl = eventUrl + '/donations/';
 var donationCountUrl = donationsUrl + 'count';
-var debugMode = true;
 var currencyKey = "";
 var currencySymbol = "";
 
@@ -18,19 +17,12 @@ function eventUI() {
     drawPageIfEventExists(eventIdentifier);
 }
 
-function debugPrint(log) {
-    if (debugMode) {
-        console.log(log);
-    }
-}
-
 function drawPageIfEventExists(eventIdentifier) {
     var url = eventExistenceUrl;
     $.getJSON(url, data => {
         if (data['event_exists']) {
             drawPage();
         } else {
-            debugPrint('Event with identifier: ' + eventIdentifier + ' does not exist');
             $('#event-alert').show();
             $('#donations-table').hide();
             $('#statistics-div').hide();
@@ -46,7 +38,6 @@ function drawPage() {
 }
 
 function getEventDetails(eventIdentifier) {
-    debugPrint('Retrieving data for event: ' + eventIdentifier);
     var url = eventUrl;
     $.getJSON(url, data => {
         setupCurrency(data);
@@ -56,61 +47,9 @@ function getEventDetails(eventIdentifier) {
     });
 }
 
-function setupCurrency(data) {
-    currencyKey = data['currency_key'];
-    // TODO: Move this to a function
-    if (currencyKey == 'EUR') {
-        currencySymbol = '€';
-    } else if (currencyKey == 'USD') {
-        currencySymbol = '$';
-    } else {
-        currencySymbol = '£';
-    }
-}
-
-// TODO: Add switch for standard, european, american timestamp order
-function convertTimestampToDatetime(timestamp) {
-    if (timestamp >= 2147483647) {
-    return 'Heat Death of the Universe';
-  }
-  var d = new Date(timestamp * 1000);
-  var day = d.getDate();
-  var month = d.getMonth() + 1;
-  var hours = d.getHours();
-  var minutes = d.getMinutes();
-  var seconds = d.getSeconds();
-  if (day < 10) {
-     day = '0' + day;
-  }
-  if (month < 10) {
-    month = '0' + month;
-  }
-  if (hours < 10) {
-    hours = '0' + hours;
-  }
-  if (minutes < 10) {
-    minutes = '0' + minutes;
-  }
-  if (seconds < 10) {
-    seconds = '0' + seconds;
-  }
-  return d.getFullYear() + '/' + month + '/' + day + ' ' + hours + ':' + minutes + ':' + seconds;
-}
-
 function drawEventDetails(data) {
-    debugPrint(data);
     $('#event-header').text(data['title']);
     $('.currency-symbol').text(currencySymbol);
-}
-
-function getEventTotal(eventIdentifier) {
-    var url = eventTotalUrl;
-    return $.getJSON(url);
-}
-
-function getLastDonation(eventIdentifier) {
-    var url = donationsUrl + '?limit=1';
-    return $.getJSON(url);
 }
 
 var newDonationUrl = apiAddress + '/donation/';
@@ -146,16 +85,12 @@ function submitDonation() {
         donor_name: donor,
         notes: notes
     }
-    debugPrint('Submitting following donation:');
-    debugPrint(data);
     request.open('POST', newDonationUrl, true);
     request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 
     // On load, handle if successful or not
     request.onload = () => {
         var response = JSON.parse(request.responseText);
-        debugPrint('Response:');
-        debugPrint(response);
         // If the donation was successful, redraw the donations
         if (response['received']) {
             getDonations(eventIdentifier);
