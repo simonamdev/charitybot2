4baeb4165e7b4b0daf259183589d98bc0b4c3e46
Simon AM <simon@agius-muscat.net>
2017-09-29 20:06:58 +0200

    Fixed total counter alignment

--- charitybot2/web/static/js/overlay/total.js
+++ charitybot2/web/static/js/overlay/total.js
@@ -3,11 +3,25 @@ var eventUrl = apiAddress + '/event/' + eventIdentifier;
 var totalUrl = eventUrl + '/total/';
 
 console.log('Connecting to API via: ' + apiAddress);
+var amountElementId = 'overlayAmount';
+var amountEl = document.getElementById(amountElementId);
 
-function getTotal() {
+getCurrencySymbol().then((symbol) => {
+    console.log('Setting up with currency symbol: ' + symbol);
+    if (symbol === '$') {
+        amountEl.classList.add('usd');
+    } else if (symbol === '€') {
+        amountEl.classList.add('eur');
+    } else if (symbol === '£') {
+        amountEl.classList.add('gbp');
+    }
+    drawTotal();
+    setInterval(drawTotal, updateDelay);
+});
+
+function drawTotal(element) {
     fetchJSONFile(totalUrl, (data) => {
-//        console.log(data);
-        var amountEl = document.getElementById('overlay-amount');
+        // console.log(data);
         amountEl.innerText = data['total'];
     }, () => {
         markUnavailable();
@@ -18,7 +32,3 @@ function markUnavailable() {
     var header = document.getElementById('overlay-amount');
     header.innerHTML = 'Connection error';
 }
-
-drawCurrencySymbolOnPage();
-getTotal();
-setInterval(getTotal, updateDelay)
