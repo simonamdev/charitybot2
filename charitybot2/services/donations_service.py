00b23744ac0f9e24c6338eab7b5f4540b0f96e64
Simon AM <simon@agius-muscat.net>
2017-10-01 22:55:37 +0200

    Added some small comments regarding functions

--- charitybot2/services/donations_service.py
+++ charitybot2/services/donations_service.py
@@ -8,18 +8,30 @@ class DonationsService:
         self._event_repository = None
         self._donations_repository = None
 
+    """
+    Opens the connections to the repositories required
+    """
     def open_connections(self):
         if self._event_repository is None:
             self._event_repository = EventSQLiteRepository(db_path=self._repository_path)
         if self._donations_repository is None:
             self._donations_repository = DonationSQLiteRepository(db_path=self._repository_path)
 
+    """
+    Closes the connections to the repositories
+    """
     def close_connections(self):
         self._event_repository.close_connection()
         self._donations_repository.close_connection()
 
+    """
+    Retrieve all donations for a given event
+    """
     def get_all_donations(self, event_identifier):
         return self._donations_repository.get_event_donations(event_identifier=event_identifier)
 
+    """
+    Retrieve the latest donation for a given event
+    """
     def get_latest_donation(self, event_identifier):
         return self._donations_repository.get_latest_event_donation(event_identifier=event_identifier)
