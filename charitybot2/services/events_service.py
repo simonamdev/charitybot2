38315f9c0daa124e4f95aba36c6bf5009b7b70cb
Simon AM <simon@agius-muscat.net>
2018-02-13 22:01:10 +0100

    Added empty methods for events service

--- charitybot2/services/events_service.py
+++ charitybot2/services/events_service.py
@@ -18,3 +18,40 @@ class EventsService:
     """
     def close_connections(self):
         self._event_repository.close_connection()
+
+    """
+    Get all registered event names
+    """
+    def get_all_events(self):
+        pass
+
+    """
+    Check if an event is registered
+    """
+    def event_is_registered(self, event_identifier):
+        pass
+
+    """
+    If an event exists, retrieve its configuration
+    """
+    def get_event_configuration(self, event_identifier):
+        pass
+
+    """
+    Register a new event
+    """
+    def register_event(self, event_identifier):
+        pass
+
+    """
+    Update an existing event
+    """
+    def update_event(self, event_identifier):
+        pass
+
+    """
+    Set the total raised for a given event
+    """
+    def set_event_total(self, event_identifier, total):
+        pass
+
