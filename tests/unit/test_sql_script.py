0f7e7e5996dcb0f80e93395bb8816dee974f32ff
Simon AM <simon@agius-muscat.net>
2018-02-13 20:52:25 +0100

    Removed old type assertion related tests

--- tests/unit/test_sql_script.py
+++ tests/unit/test_sql_script.py
@@ -5,18 +5,6 @@ from charitybot2.persistence.sql_script import SQLScript
 
 
 class TestSQLScript:
-    @pytest.mark.parametrize('path', [
-        1,
-        1.5,
-        object,
-        [],
-        (),
-        None
-    ])
-    def test_passing_incorrect_arguments_throws_exceptions(self, path):
-        with pytest.raises(IllegalArgumentException):
-            sql_script = SQLScript(path=path)
-
     def test_passing_non_existent_file_throws_exception(self):
         with pytest.raises(FileNotFoundError):
             sql_script = SQLScript(path='bla/bla.sql')
