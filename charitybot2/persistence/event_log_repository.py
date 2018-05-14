997e4d4603df63dedb13bd9f89484f220aed9429
Simon AM <simon@agius-muscat.net>
2018-05-14 20:55:49 +0200

    Added several event log tests

--- charitybot2/persistence/event_log_repository.py
+++ charitybot2/persistence/event_log_repository.py
@@ -12,6 +12,14 @@ def fix_placeholders(query):
     return str(query).replace('\'', '')
 
 
+class EventLogAlreadyExistsException(Exception):
+    pass
+
+
+class EventLogDoesNotExistException(Exception):
+    pass
+
+
 class EventLogRepository(SQLiteRepository):
     def __init__(self, db_path='memory', debug=False):
         super().__init__(db_path=db_path, debug=debug)
@@ -22,10 +30,22 @@ class EventLogRepository(SQLiteRepository):
         init_script = SQLScript(path=init_event_log_script_path)
         self.execute_query(query=init_script.return_sql(), commit=True)
 
+    def event_log_exists(self, event_identifier):
+        return None
+
     def get_logged_events(self):
         q = Query.from_(self._event_log_table).select('internalName')
         rows = self.execute_query(query=fix_placeholders(q)).fetchall()
         return [self.__convert_row_to_event_log(row) for row in rows]
+
+    def add_event_log(self, event_identifier, timestamp=None):
+        return None
+
+    def update_event_log(self, event_identifier, timestamp):
+        return None
+
+    def get_event_log_timestamp(self, event_identifier):
+        return None
 
     @staticmethod
     def __convert_row_to_event_log(row):
--- tests/integration/test_event_log_repository.py
+++ tests/integration/test_event_log_repository.py
@@ -1,8 +1,13 @@
-from charitybot2.models.event_log import EventLog
-from charitybot2.persistence.event_log_repository import EventLogRepository
+import random
 
+import pytest
+from charitybot2.models.event_log import EventLog
+from charitybot2.persistence.event_log_repository import EventLogRepository, EventLogAlreadyExistsException, \
+    EventLogDoesNotExistException
 
 test_event_identifier = 'test_event'
+non_existent_event_identifier = 'blalbalblabla'
+test_event_log_time = 123
 
 
 class TestEventLogRepository:
@@ -14,18 +19,72 @@ class TestEventLogRepository:
     def teardown_method(self):
         self.test_event_log_repository.close_connection()
 
+    def test_check_if_non_existent_event_log_exists(self):
+        assert False is self.test_event_log_repository.event_log_exists(event_identifier=non_existent_event_identifier)
+
+    def test_check_if_existing_event_log_exists(self):
+        self.test_event_log_repository.add_event_log(
+            event_identifier=test_event_identifier,
+            timestamp=test_event_log_time
+        )
+        assert True is self.test_event_log_repository.event_log_exists(event_identifier=test_event_identifier)
+
     def test_retrieving_logged_events(self):
         logged_events = self.test_event_log_repository.get_logged_events()
         assert 0 == len(logged_events)
-        # Add event log
-        # TODO
+        self.test_event_log_repository.add_event_log(
+            event_identifier=test_event_identifier,
+            timestamp=test_event_log_time
+        )
         logged_events = self.test_event_log_repository.get_logged_events()
         assert 1 == len(logged_events)
         assert isinstance(logged_events[0], EventLog)
         assert test_event_identifier == logged_events[0].identifier
-        test_event_log_time = 0
         assert test_event_log_time == logged_events[0].last_log_time
 
     def test_retrieving_logged_events_with_no_events_present(self):
         logged_events = self.test_event_log_repository.get_logged_events()
         assert 0 == len(logged_events)
+
+    def test_adding_new_event_log(self):
+        new_event_log_event_identifier = 'new_event'
+        assert False is self.test_event_log_repository.event_log_exists(event_identifier=new_event_log_event_identifier)
+        self.test_event_log_repository.add_event_log(
+            event_identifier=new_event_log_event_identifier,
+            timestamp=test_event_log_time)
+        assert True is self.test_event_log_repository.event_log_exists(event_identifier=new_event_log_event_identifier)
+
+    def test_adding_new_event_log_when_it_already_exists(self):
+        existing_event_log_event_identifier = 'this_event_already exists'
+        existing_time = test_event_log_time + random.randint(5, 10)
+        self.test_event_log_repository.add_event_log(
+            event_identifier=existing_event_log_event_identifier,
+            timestamp=existing_time
+        )
+        with pytest.raises(EventLogAlreadyExistsException):
+            self.test_event_log_repository.add_event_log(
+                event_identifier=existing_event_log_event_identifier,
+                timestamp=existing_time
+            )
+
+    def test_updating_existing_event_log(self):
+        event_log_being_updated_identifier = 'I am Being Updooted'
+        self.test_event_log_repository.add_event_log(
+            event_identifier=event_log_being_updated_identifier,
+            timestamp=test_event_log_time
+        )
+        new_time = test_event_log_time + random.randint(100, 200)
+        self.test_event_log_repository.update_event_log(
+            event_identifier=event_log_being_updated_identifier,
+            timestamp=new_time
+        )
+        assert new_time == self.test_event_log_repository.get_event_log_timestamp(
+            event_identifier=event_log_being_updated_identifier
+        )
+
+    def test_updating_non_existent_event_log(self):
+        with pytest.raises(EventLogDoesNotExistException):
+            self.test_event_log_repository.update_event_log(
+                event_identifier=non_existent_event_identifier,
+                timestamp=1010101001
+            )
